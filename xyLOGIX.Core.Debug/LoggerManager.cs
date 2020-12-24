8018ab3f5ece7bf3bffa56a0cb17ebf229cb6580
Brian Hart <brian.hart@xylogix.net>
2020-12-24 12:00:56 -0500

    Convert methods to expression body

--- xyLOGIX.Core.Debug/LoggerManager.cs
+++ xyLOGIX.Core.Debug/LoggerManager.cs
@@ -3,25 +3,20 @@
 namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Provides methods to access objects of type <see cref="T:log4net.Hierarchy.Repository.Logger"/> from log4net.
+    ///     Provides methods to access objects of type
+    ///     <see cref="T:log4net.Hierarchy.Repository.Logger" /> from log4net.
     /// </summary>
     public static class LoggerManager
     {
         /// <summary>
-        /// Gets a reference to the default logger repository's root instance of <see cref="T:log4net.Hierarchy.Repository.Logger"/>.
+        ///     Gets a reference to the default logger repository's root instance of
+        ///     <see cref="T:log4net.Hierarchy.Repository.Logger" />.
         /// </summary>
-        /// <returns>Reference to the default logger repository's root instance of <see cref="T:log4net.Hierarchy.Repository.Logger"/>, or null if not found.</returns>
-        public static Logger GetRootLogger()
-        {
-            Logger result = null;
-
-            var repo = LoggerRepositoryManager.GetHierarchyRepository();
-            if (repo == null)
-                return result;
-
-            result = repo.Root;
-
-            return result;
-        }
+        /// <returns>
+        ///     Reference to the default logger repository's root instance of
+        ///     <see cref="T:log4net.Hierarchy.Repository.Logger" />, or null if not found.
+        /// </returns>
+        public static Logger GetRootLogger() =>
+            LoggerRepositoryManager.GetHierarchyRepository()?.Root;
     }
 }
