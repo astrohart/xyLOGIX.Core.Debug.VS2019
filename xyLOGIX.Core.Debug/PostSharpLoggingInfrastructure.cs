bc70b173c97f532e6ab8b6ad36331bfa60aed93c
Brian Hart <brian.hart@xylogix.net>
2020-12-24 11:58:13 -0500

    Override Type property
    The new Type property in this class returns LoggingInfrastructureType.PostSharp.

--- xyLOGIX.Core.Debug/PostSharpLoggingInfrastructure.cs
+++ xyLOGIX.Core.Debug/PostSharpLoggingInfrastructure.cs
@@ -1,6 +1,17 @@
 ﻿namespace xyLOGIX.Core.Debug
 {
-    public class
-        PostSharpLoggingInfrastructure : DefaultLoggingInfrastructure
-    { }
+    /// <summary>
+    ///     Implements log-file management for the case when we are utilizing PostSharp
+    ///     aspects to handle the bulk of logging for us.
+    /// </summary>
+    public class PostSharpLoggingInfrastructure : DefaultLoggingInfrastructure
+    {
+        /// <summary>
+        ///     Gets the <see cref="T:xyLOGIX.Core.Debug.LoggingInfrastructureType" />
+        ///     value that corresponds to the type of infrastructure that is being
+        ///     utilized.
+        /// </summary>
+        public override LoggingInfrastructureType Type =>
+            LoggingInfrastructureType.PostSharp;
+    }
 }
