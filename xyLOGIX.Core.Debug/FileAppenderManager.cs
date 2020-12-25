57a85de0d890af60baa27f52f89fb66524a75deb
Brian Hart <brian.hart@xylogix.net>
2020-12-25 09:25:43 -0500

    Edit GetFirstAppender to accept an optional loggerRepository parameter
    This method is adapted to accept an optional loggerRepository parameter for getting the root logger from.  This is to support compatibility with PostSharp.

--- xyLOGIX.Core.Debug/FileAppenderManager.cs
+++ xyLOGIX.Core.Debug/FileAppenderManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using log4net.Appender;
+using log4net.Repository;
 
 namespace xyLOGIX.Core.Debug
 {
@@ -12,21 +13,20 @@ namespace xyLOGIX.Core.Debug
     {
         public static FileAppender GetAppenderByName(string name)
         {
-            // Check to see if the required parameter, , is blank, whitespace, or null. If it is any of these, throw an
-            // ArgumetNullException.
+            // Check to see if the required parameter, name, is blank,
+            // whitespace, or null. If it is any of these, throw an
+            // ArgumentNullException.
 
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentNullException(nameof(name));
 
-            FileAppender result = null;
-
             var root = LoggerManager.GetRootLogger();
             if (root == null)
-                return result;
+                return null;
 
             return root.Appenders.Count == 0
-                ? result
-                : root.Appenders.Cast<FileAppender>()
+                ? null
+                : root.Appenders.OfType<FileAppender>()
                     .First(fa => fa.Name == name);
         }
 
@@ -34,19 +34,21 @@ namespace xyLOGIX.Core.Debug
         ///     If the root logger's appenders list contains appenders, returns a reference
         ///     to the first one in the list.
         /// </summary>
+        /// <param name="loggerRepository"></param>
         /// <returns>
         ///     Reference to an instance of
         ///     <see cref="T:log4net.Appender.FileAppender" />, or null if not found.
         /// </returns>
-        public static FileAppender GetFirstAppender()
+        public static FileAppender GetFirstAppender(
+            ILoggerRepository loggerRepository = null)
         {
-            var root = LoggerManager.GetRootLogger();
+            var root = LoggerManager.GetRootLogger(loggerRepository);
             if (root == null)
                 return null;
 
             return root.Appenders.Count == 0
                 ? null
-                : root.Appenders.Cast<FileAppender>().First();
+                : root.Appenders.OfType<FileAppender>().First();
         }
     }
 }
