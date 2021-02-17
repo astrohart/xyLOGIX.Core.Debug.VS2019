7e1658df45a958bbc03c674dabd14e740b7f57b3
Brian Hart <brian.hart@xylogix.net>
2021-02-17 10:13:30 -0500

    Add XML docs

--- xyLOGIX.Core.Debug/DebugLevel.cs
+++ xyLOGIX.Core.Debug/DebugLevel.cs
@@ -1,10 +1,30 @@
 ﻿namespace xyLOGIX.Core.Debug
 {
-   public enum DebugLevel
-   {
-      Info,
-      Warning,
-      Error,
-      Debug
-   }
+    /// <summary>
+    /// Values to indicate the level of logging message to be utilized for the
+    /// debugging log.
+    /// </summary>
+    public enum DebugLevel
+    {
+        /// <summary>
+        /// Informational message to be displayed even in Release mode.
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// Warning that indicates a potential risk that has not yet become an issue.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// Error that indicates an issue that has led to the stoppage of an
+        /// operation or the software as a whole.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Informational message that may only be visible from a Debug mode executable.
+        /// </summary>
+        Debug
+    }
 }
