2f222af53f314c450714a9d97ae88b50538dbede
Brian Hart <brian.hart@xylogix.net>
2023-08-13 19:13:07 -0600

    Latest updates at 19:13 hours MDT on 08/13/2023

---
+++ xyLOGIX.Core.Debug/ExceptionLoggedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Provides information for <c>ExceptionLogged</c> event handlers.
+    /// </summary>
+    public class ExceptionLoggedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:xyLOGIX.Core.Debug.ExceptionLoggedEventArgs" /> and returns a
+        /// reference to it.
+        /// </summary>
+        /// <param name="exception">
+        /// (Required.) Reference to an instance of the <see cref="T:System.Exception" />
+        /// that was logged.
+        /// </param>
+        public ExceptionLoggedEventArgs(Exception exception)
+            => Exception = exception;
+
+        /// <summary>
+        /// Gets or sets a reference to an instance of the
+        /// <see cref="T:System.Exception" /> that was logged.
+        /// </summary>
+        public Exception Exception { get; }
+    }
+}
---
+++ xyLOGIX.Core.Debug/ExceptionLoggedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Represents a handler for a <c>ExceptionLogged</c> event.
+    /// </summary>
+    /// <param name="sender">
+    /// Reference to the instance of the object that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// A <see cref="T:xyLOGIX.Core.Debug.ExceptionLoggedEventArgs" /> that contains
+    /// the event data.
+    /// </param>
+    /// <remarks>
+    /// This delegate merely specifies the signature of all methods that handle the
+    /// <c>ExceptionLogged</c> event.
+    /// </remarks>
+    public delegate void ExceptionLoggedEventHandler(
+        object sender,
+        ExceptionLoggedEventArgs e
+    );
+}
