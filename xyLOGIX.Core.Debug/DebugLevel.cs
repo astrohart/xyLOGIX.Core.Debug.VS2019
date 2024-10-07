7218a62f4232b967cb0817ec5df90128c4f7a4ed
Brian Hart <brian.hart@xylogix.net>
2024-10-07 00:57:33 -0600

    Update DebugLevel.cs

--- xyLOGIX.Core.Debug/DebugLevel.cs
+++ xyLOGIX.Core.Debug/DebugLevel.cs
@@ -1,18 +1,8 @@
 namespace xyLOGIX.Core.Debug
-{
-    /// <summary> Identifies the debug message level for a logging message. </summary>
-    public enum DebugLevel
-    {
-        /// <summary> The logging message is for debugging purposes only. </summary>
-        Debug,
-
-        /// <summary> The logging message is for warning purposes only. </summary>
-        Warning,
-
-        /// <summary> The logging message is for error purposes only. </summary>
-        Error,
-
-        /// <summary> The logging message is for informational purposes only. </summary>
-        Info
-    }
-}
+{ /// <summary> Identifies the debug message level for a logging message. </summary>
+    public enum DebugLevel { /// <summary> The logging message is for debugging purposes only. </summary>
+        Debug, /// <summary> The logging message is for warning purposes only. </summary>
+        Warning, /// <summary> The logging message is for error purposes only. </summary>
+        Error, /// <summary> The logging message is for informational purposes only. </summary>
+        Info, /// <summary> Unknown logging level.</summary>
+        Unknown = -1 } }
