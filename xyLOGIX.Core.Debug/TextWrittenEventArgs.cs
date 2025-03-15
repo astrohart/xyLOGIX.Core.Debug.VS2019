4ca74ad7f29a9616e04c60061b4cfbb97d05e117
Brian Hart <brian.hart@xylogix.net>
2025-03-15 10:22:25 -0600

    Update TextWrittenEventArgs.cs
    * Refactor logging attributes and improve documentation.
    * Removed `Log(AttributeExclude = true)` from class declaration.
      - Added it to the static constructor for simplified logging.
    * Modified constructor to include `[NotLogged]` for the `text` parameter.
      - This indicates that the parameter should not be logged.
    * Added XML documentation comments for clarity on logging attributes.

--- xyLOGIX.Core.Debug/TextWrittenEventArgs.cs
+++ xyLOGIX.Core.Debug/TextWrittenEventArgs.cs
@@ -6,16 +6,31 @@ using System.Diagnostics;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary> Provides information for <c>TextWritten</c> event handlers. </summary>
-    [ExplicitlySynchronized, Log(AttributeExclude = true)]
+    [ExplicitlySynchronized]
     public class TextWrittenEventArgs : EventArgs
     {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the <see cref="T:xyLOGIX.Core.Debug.TextWrittenEventArgs" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static TextWrittenEventArgs() { }
+
         /// <summary>
         /// Constructs a new instance of
         /// <see cref="T:xyLOGIX.Core.Debug.TextWrittenEventArgs" /> and returns a
         /// reference to it.
         /// </summary>
         /// <param name="text">(Required.) String containing the text to be written.</param>
-        public TextWrittenEventArgs(string text)
+        [Log(AttributeExclude = true)]
+        public TextWrittenEventArgs([NotLogged] string text)
             => Text = text;
 
         /// <summary> Gets a string containing the text to be written. </summary>
