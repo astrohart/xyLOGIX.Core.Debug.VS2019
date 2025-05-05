efacd82d425a3af3fcf025480528183e3ef8c9d2
Brian Hart <brian.hart@xylogix.net>
2025-05-04 19:30:33 -0600

    Create GetOutputLocationTypeValidator.cs
    - Introduce `GetOutputLocationTypeValidator` static class.
    - Implement singleton pattern for `IOutputLocationTypeValidator`.
    - Define `IOutputLocationTypeValidator` interface with `IsValid` method.
    - Update `OutputLocationTypeValidator` to implement the interface.
    - Add logging and exception handling in validation logic.

---
+++ xyLOGIX.Core.Debug/IOutputLocationTypeValidator.cs
@@ -0,0 +1,31 @@
+﻿namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a validator of
+    /// <see cref="T:xyLOGIX.Core.Debug.OutputLocationType" /> enumeration values.
+    /// </summary>
+    /// <remarks>
+    /// Specifically, objects that implement this interface ascertain whether
+    /// the values of variables fall within the value set that is defined by the
+    /// <see cref="T:xyLOGIX.Core.Debug.OutputLocationType" /> enumeration.
+    /// </remarks>
+    public interface IOutputLocationTypeValidator
+    {
+        /// <summary>
+        /// Determines whether the output location <paramref name="type" /> value
+        /// passed is within the value set that is defined by the
+        /// <see cref="T:xyLOGIX.Core.Debug.OutputLocationType" /> enumeration.
+        /// </summary>
+        /// <param name="type">
+        /// (Required.) One of the
+        /// <see cref="T:xyLOGIX.Core.Debug.OutputLocationType" /> values that is to be
+        /// examined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the output location
+        /// <paramref name="type" /> falls within the defined value set;
+        /// <see langword="false" /> otherwise.
+        /// </returns>
+        bool IsValid(OutputLocationType type);
+    }
+}
