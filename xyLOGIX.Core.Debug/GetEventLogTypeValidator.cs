b8607a0acad4b27501c84a744304d13269a8b8ab
Brian Hart <brian.hart@xylogix.net>
2025-05-04 19:29:26 -0600

    Create GetEventLogTypeValidator.cs and IEventLogTypeValidator.cs
    - Added static class `GetEventLogTypeValidator` to provide a singleton instance of `IEventLogTypeValidator`.
      - Implemented a static constructor for initialization.
      - Added `SoleInstance()` method with exception handling for instance retrieval.
    
    - Introduced `IEventLogTypeValidator` interface.
      - Defined `IsValid()` method to validate `EventLogType` values.
      - Intended for implementation by event log type validation classes.

---
+++ xyLOGIX.Core.Debug/GetEventLogTypeValidator.cs
@@ -0,0 +1,55 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:xyLOGIX.Core.Debug.IEventLogTypeValidator" /> interface.
+    /// </summary>
+    public static class GetEventLogTypeValidator
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:xyLOGIX.Core.Debug.GetEventLogTypeValidator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetEventLogTypeValidator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IEventLogTypeValidator" /> interface, and
+        /// returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IEventLogTypeValidator" /> interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        public static IEventLogTypeValidator SoleInstance()
+        {
+            IEventLogTypeValidator result;
+
+            try
+            {
+                result = EventLogTypeValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the Debug output.
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ xyLOGIX.Core.Debug/IEventLogTypeValidator.cs
@@ -0,0 +1,31 @@
+﻿using PostSharp.Patterns.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a validator of
+    /// <see cref="T:xyLOGIX.Core.Debug.EventLogType" /> enumeration values.
+    /// </summary>
+    /// <remarks>
+    /// Specifically, objects that implement this interface ascertain whether
+    /// the values of variables fall within the value set that is defined by the
+    /// <see cref="T:xyLOGIX.Core.Debug.EventLogType" /> enumeration.
+    /// </remarks>
+    public interface IEventLogTypeValidator
+    {
+        /// <summary>
+        /// Determines whether the event log <paramref name="type" /> value passed
+        /// is within the value set that is defined by the
+        /// <see cref="T:xyLOGIX.Core.Debug.EventLogType" /> enumeration.
+        /// </summary>
+        /// <param name="type">
+        /// (Required.) One of the
+        /// <see cref="T:xyLOGIX.Core.Debug.EventLogType" /> values that is to be examined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the event log <paramref name="type" />
+        /// falls within the defined value set; <see langword="false" /> otherwise.
+        /// </returns>
+        bool IsValid([NotLogged] EventLogType type);
+    }
+}
