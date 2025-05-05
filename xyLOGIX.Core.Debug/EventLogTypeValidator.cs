2c9230b0d3c2e61afaf46eaa15ce680cd9d977d2
Brian Hart <brian.hart@xylogix.net>
2025-05-04 19:28:54 -0600

    Create EventLogTypeValidator.cs
    - Introduce `EventLogTypeValidator` class in the `xyLOGIX.Core.Debug` namespace.
    - Implement `IEventLogTypeValidator` interface for validating `EventLogType` values.
    - Include static constructors to prevent direct instantiation.
    - Add a singleton instance property for easy access.
    - Implement `IsValid` method to check the validity of `EventLogType` values with detailed logging for debugging.

---
+++ xyLOGIX.Core.Debug/EventLogTypeValidator.cs
@@ -0,0 +1,145 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Validates whether certain value(s) are within the defined value set of the
+    /// <see cref="T:xyLOGIX.Core.Debug.EventLogType" /> enumeration.
+    /// </summary>
+    public class EventLogTypeValidator : IEventLogTypeValidator
+    {
+        /// <summary>Empty, static constructor to prohibit direct allocation of this class.</summary>
+        [Log(AttributeExclude = true)]
+        static EventLogTypeValidator() { }
+
+        /// <summary>
+        /// Empty, protected constructor to prohibit direct allocation of this
+        /// class.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        protected EventLogTypeValidator() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that
+        /// implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.Interfaces.IEventLogTypeValidator" />
+        /// interface.
+        /// </summary>
+        public static IEventLogTypeValidator Instance
+        {
+            [DebuggerStepThrough] get;
+        } = new EventLogTypeValidator();
+
+        /// <summary>
+        /// Determines whether the event log <paramref name="type" /> value passed
+        /// is within the value set that is defined by the
+        /// <see cref="T:xyLOGIX.Core.Debug.EventLogType" /> enumeration.
+        /// </summary>
+        /// <param name="type">
+        /// (Required.) One of the
+        /// <see cref="T:xyLOGIX.Core.Debug.EventLogType" /> values that is to be examined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the event log <paramref name="type" />
+        /// falls within the defined value set; <see langword="false" /> otherwise.
+        /// </returns>
+        public bool IsValid(EventLogType type)
+        {
+            var result = false;
+
+            try
+            {
+                // Dump the argument of the parameter, 'type', to the log
+                System.Diagnostics.Debug.WriteLine(
+                    $"EventLogTypeValidator.IsValid: type = '{type}'"
+                );
+
+                /*
+                 * For cybersecurity reasons, and to defeat reverse-engineering,
+                 * check the value of the 'type' parameter to ensure that it
+                 * is not set to a value outside the set of valid values defined
+                 * by the xyLOGIX.Core.Debug.EventLogType
+                 * enumeration.
+                 *
+                 * In principle, since all C# enums devolve to integer values, a
+                 * hacker could insert a different value into the CPU register that the
+                 * 'type' parameter is read from and thereby make this application
+                 * do something it's not intended to do.
+                 */
+
+                System.Diagnostics.Debug.WriteLine(
+                    $"EventLogTypeValidator.IsValid: Checking whether the value of the 'type' parameter, i.e., '{type}', is within the defined value set of its enumerated data type..."
+                );
+
+                // Check whether the value of the 'type' parameter is within the defined value set of its 
+                // enumeration data type.  If this is not the case, then write an error message to the log
+                // file, and then terminate the execution of this method while returning the default return
+                // value.
+                if (!Enum.IsDefined(typeof(EventLogType), type))
+                {
+                    // The value of the 'type' parameter is NOT within the defined value set for its enumerated data type.  This is not desirable.
+                    System.Diagnostics.Debug.WriteLine(
+                        $"*** ERROR *** The value of the 'type' parameter, i.e., '{type}', is NOT within the defined value set of its enumerated data type.  Stopping..."
+                    );
+
+                    System.Diagnostics.Debug.WriteLine(
+                        $"EventLogTypeValidator.IsValid: Result = {result}"
+                    );
+
+                    // stop.
+                    return result;
+                }
+
+                System.Diagnostics.Debug.WriteLine(
+                    $"EventLogTypeValidator.IsValid: *** SUCCESS *** The value of the 'type' parameter, i.e., '{type}', is within the defined value set of its enumerated data type.  Proceeding..."
+                );
+
+                System.Diagnostics.Debug.WriteLine(
+                    "EventLogTypeValidator.IsValid: Checking whether neither the 'Unknown' nor the 'None' value has been specified for the 'type' parameter..."
+                );
+
+                // Check whether either the 'Unknown' or the 'None' value(s) have been specified
+                // for the argument of the 'type' parameter.  If either of these values are the
+                // specified argument, then this is unacceptable -- we will write an error message
+                // to the Debug output, and then terminate the execution of this method while
+                // returning FALSE, in this event.  Otherwise, the method execution will proceed.
+                if (EventLogType.Unknown.Equals(type) ||
+                    EventLogType.None.Equals(type))
+                {
+                    // Either the 'Unknown' or the 'None' value has been specified for the 'type' parameter.  This is not desirable.
+                    System.Diagnostics.Debug.WriteLine(
+                        "*** ERROR *** Either the 'Unknown' or the 'None' value has been specified for the 'type' parameter.  Stopping..."
+                    );
+
+                    System.Diagnostics.Debug.WriteLine(
+                        $"EventLogTypeValidator.IsValid: Result = {result}"
+                    );
+
+                    // stop.
+                    return result;
+                }
+
+                System.Diagnostics.Debug.WriteLine(
+                    "EventLogTypeValidator.IsValid: *** SUCCESS *** The 'Unknown' value has NOT been specified for the 'type' parameter.  Proceeding..."
+                );
+
+                /*
+                 * If we made it here, then assume that the input data is valid.
+                 */
+
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the Debug output.
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
