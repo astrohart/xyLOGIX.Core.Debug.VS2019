bcae5c91a919ccc11e893a4dc9706c4678c7be6e
Brian Hart <brian.hart@xylogix.net>
2025-05-04 19:29:55 -0600

    Updating GetFromConfigFileLoggingConfigurator.cs
    - Added a line to log exception details to system debug output.
    - Enhanced error logging by providing additional visibility into exceptions.

--- xyLOGIX.Core.Debug/GetFromConfigFileLoggingConfigurator.cs
+++ xyLOGIX.Core.Debug/GetFromConfigFileLoggingConfigurator.cs
@@ -48,7 +48,7 @@ namespace xyLOGIX.Core.Debug
             catch (Exception ex)
             {
                 // dump all the exception info to the log
-                DebugUtils.LogException(ex);
+                System.Diagnostics.Debug.WriteLine(ex);
 
                 result = default;
             }
