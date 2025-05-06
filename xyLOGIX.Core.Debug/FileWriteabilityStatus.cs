637732823300d47f35f1b952a591ad7c03c84ef0
Brian Hart <brian.hart@xylogix.net>
2025-05-06 01:18:37 -0600

    Create FileWriteabilityStatus.cs
    - Introduce new namespace `xyLOGIX.Core.Debug`
    - Define enumeration `FileWriteabilityStatus`
      - Members: `NoDetermination`, `NotWriteable`, `Writeable`, `Unknown`
      - Each member includes a summary comment
      - `Unknown` member assigned a value of `-1`

---
+++ xyLOGIX.Core.Debug/FileWriteabilityStatus.cs
@@ -0,0 +1,29 @@
+﻿namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Value(s) that indicate whether a file is writeable or not, or whether it cannot
+    /// be determined from the information available.
+    /// </summary>
+    public enum FileWriteabilityStatus
+    {
+        /// <summary>
+        /// No determination could be made as to whether the file is writeable or not.
+        /// </summary>
+        NoDetermination,
+
+        /// <summary>
+        /// The file is not writeable by the currently-logged-in user.
+        /// </summary>
+        NotWriteable,
+
+        /// <summary>
+        /// The file is writeable by the currently-logged-in user.
+        /// </summary>
+        Writeable,
+
+        /// <summary>
+        /// Unknown writeability status.
+        /// </summary>
+        Unknown = -1
+    }
+}
