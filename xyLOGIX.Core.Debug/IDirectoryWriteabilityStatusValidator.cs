8e51a413e013ab2125bc69afd81e11e560d8e767
Brian Hart <brian.hart@xylogix.net>
2025-05-06 01:18:58 -0600

    Create IDirectoryWriteabilityStatusValidator.cs
    - Added a new interface `IDirectoryWriteabilityStatusValidator` in the `xyLOGIX.Core.Debug` namespace.
    - Defined the method `IsValid` to validate `DirectoryWriteabilityStatus` values.
    - Included XML documentation for clarity on purpose, parameters, and return values.

---
+++ xyLOGIX.Core.Debug/IDirectoryWriteabilityStatusValidator.cs
@@ -0,0 +1,33 @@
+﻿namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a validator of
+    /// <see cref="T:xyLOGIX.Core.Debug.DirectoryWriteabilityStatus" /> enumeration
+    /// values.
+    /// </summary>
+    /// <remarks>
+    /// Specifically, objects that implement this interface ascertain whether
+    /// the values of variables fall within the value set that is defined by the
+    /// <see cref="T:xyLOGIX.Core.Debug.DirectoryWriteabilityStatus" /> enumeration.
+    /// </remarks>
+    public interface IDirectoryWriteabilityStatusValidator
+    {
+        /// <summary>
+        /// Determines whether the directory writeability
+        /// <paramref name="status" /> value passed is within the value set that is defined
+        /// by the <see cref="T:xyLOGIX.Core.Debug.DirectoryWriteabilityStatus" />
+        /// enumeration.
+        /// </summary>
+        /// <param name="status">
+        /// (Required.) One of the
+        /// <see cref="T:xyLOGIX.Core.Debug.DirectoryWriteabilityStatus" /> values that is
+        /// to be examined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the directory writeability
+        /// <paramref name="status" /> falls within the defined value set;
+        /// <see langword="false" /> otherwise.
+        /// </returns>
+        bool IsValid(DirectoryWriteabilityStatus status);
+    }
+}
