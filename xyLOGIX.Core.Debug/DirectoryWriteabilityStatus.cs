95e01c5fac1a1626109980029ca6fa6f2b0f8d36
Brian Hart <brian.hart@xylogix.net>
2025-05-06 01:18:47 -0600

    Create DirectoryWriteabilityStatus.cs
    - Added a new enum `DirectoryWriteabilityStatus` to the `xyLOGIX.Core.Debug` namespace.
    - Enum values indicate folder writeability status:
      - `NoDetermination`: No determination could be made.
      - `NotWriteable`: The folder is not writable by the current user.
      - `Writeable`: The folder is writable by the current user.
      - `Unknown`: Unknown writeability status, set to -1.

---
+++ xyLOGIX.Core.Debug/DirectoryWriteabilityStatus.cs
@@ -0,0 +1,29 @@
+﻿namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Value(s) that indicate whether a folder is writeable or not, or whether it cannot
+    /// be determined from the information available.
+    /// </summary>
+    public enum DirectoryWriteabilityStatus
+    {
+        /// <summary>
+        /// No determination could be made as to whether the folder is writeable or not.
+        /// </summary>
+        NoDetermination,
+
+        /// <summary>
+        /// The folder is not writeable by the currently-logged-in user.
+        /// </summary>
+        NotWriteable,
+
+        /// <summary>
+        /// The folder is writeable by the currently-logged-in user.
+        /// </summary>
+        Writeable,
+
+        /// <summary>
+        /// Unknown writeability status.
+        /// </summary>
+        Unknown = -1
+    }
+}
