e11c307a5830ae3695343dd765209686f7f1405f
Brian Hart <brian.hart@xylogix.net>
2025-05-06 01:19:41 -0600

    Create GetDirectoryWriteabilityStatusValidator.cs
    - Introduced a new static class `GetDirectoryWriteabilityStatusValidator` in the `xyLOGIX.Core.Debug` namespace.
    - Provides a singleton instance of `IDirectoryWriteabilityStatusValidator`.
    - Includes a static constructor for initialization.
    - Implements the `SoleInstance()` method to return the validator instance.
    - Handles exceptions by logging them to the Debug output.

---
+++ xyLOGIX.Core.Debug/GetDirectoryWriteabilityStatusValidator.cs
@@ -0,0 +1,58 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:xyLOGIX.Core.Debug.IDirectoryWriteabilityStatusValidator" />
+    /// interface.
+    /// </summary>
+    public static class GetDirectoryWriteabilityStatusValidator
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.GetDirectoryWriteabilityStatusValidator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetDirectoryWriteabilityStatusValidator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IDirectoryWriteabilityStatusValidator" />
+        /// interface, and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IDirectoryWriteabilityStatusValidator" />
+        /// interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        public static IDirectoryWriteabilityStatusValidator SoleInstance()
+        {
+            IDirectoryWriteabilityStatusValidator result;
+
+            try
+            {
+                result = DirectoryWriteabilityStatusValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the Debug output
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
