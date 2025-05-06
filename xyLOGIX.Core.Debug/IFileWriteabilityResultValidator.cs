1b28faa6dbd1de154fa19db903314377b6321422
Brian Hart <brian.hart@xylogix.net>
2025-05-06 01:19:08 -0600

    Create IFileWriteabilityResultValidator.cs
    - Introduce the `IFileWriteabilityStatusValidator` interface.
    - Define the `IsValid` method to validate `FileWriteabilityStatus`.
    - Add detailed documentation for the interface and method.

---
+++ xyLOGIX.Core.Debug/IFileWriteabilityResultValidator.cs
@@ -0,0 +1,31 @@
+﻿namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a validator of
+    /// <see cref="T:xyLOGIX.Core.Debug.FileWriteabilityStatus" /> enumeration values.
+    /// </summary>
+    /// <remarks>
+    /// Specifically, objects that implement this interface ascertain whether
+    /// the values of variables fall within the value set that is defined by the
+    /// <see cref="T:xyLOGIX.Core.Debug.FileWriteabilityStatus" /> enumeration.
+    /// </remarks>
+    public interface IFileWriteabilityStatusValidator
+    {
+        /// <summary>
+        /// Determines whether the file writeability <paramref name="status" />
+        /// value passed is within the value set that is defined by the
+        /// <see cref="T:xyLOGIX.Core.Debug.FileWriteabilityStatus" /> enumeration.
+        /// </summary>
+        /// <param name="status">
+        /// (Required.) One of the
+        /// <see cref="T:xyLOGIX.Core.Debug.FileWriteabilityStatus" /> values that is to be
+        /// examined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the file writeability
+        /// <paramref name="status" /> falls within the defined value set;
+        /// <see langword="false" /> otherwise.
+        /// </returns>
+        bool IsValid(FileWriteabilityStatus status);
+    }
+}
