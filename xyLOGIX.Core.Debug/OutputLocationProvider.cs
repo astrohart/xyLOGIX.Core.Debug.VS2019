5c75d93eb816c30be2bca89dff98431809e60e5a
Brian Hart <brian.hart@xylogix.net>
2025-05-11 17:52:14 -0600

    Remove Yielder attribute from OutputLocationProvider.cs
    - Modified the `OnMuteConsoleChanged` method.
    - Removed the `[Yielder]` attribute.
    - This change may impact asynchronous control flow.

--- xyLOGIX.Core.Debug/OutputLocationProvider.cs
+++ xyLOGIX.Core.Debug/OutputLocationProvider.cs
@@ -697,7 +697,6 @@ namespace xyLOGIX.Core.Debug
         /// contains
         /// the event data.
         /// </param>
-        [Yielder]
         protected virtual void OnMuteConsoleChanged(
             [NotLogged] MuteConsoleChangedEventArgs e
         )
