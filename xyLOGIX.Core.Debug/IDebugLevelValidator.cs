8048c40d984e237919acf272fb3ca6b6f9c51dc9
Brian Hart <brian.hart@xylogix.net>
2025-05-11 19:05:57 -0600

    Create IDebugLevelValidator.cs
    - Introduce the `IDebugLevelValidator` interface in the `xyLOGIX.Core.Debug` namespace.
    - Define the `IsValid` method to validate `DebugLevel` values.
    - Add comprehensive documentation for the interface and method, including:
      - Purpose of the interface.
      - Parameter descriptions.
      - Return value explanations.

---
+++ xyLOGIX.Core.Debug/IDebugLevelValidator.cs
@@ -0,0 +1,29 @@
+﻿namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a validator of
+    /// <see cref="T:xyLOGIX.Core.Debug.DebugLevel" /> enumeration values.
+    /// </summary>
+    /// <remarks>
+    /// Specifically, objects that implement this interface ascertain whether
+    /// the values of variables fall within the value set that is defined by the
+    /// <see cref="T:xyLOGIX.Core.Debug.DebugLevel" /> enumeration.
+    /// </remarks>
+    public interface IDebugLevelValidator
+    {
+        /// <summary>
+        /// Determines whether the debug <paramref name="level" /> value passed is
+        /// within the value set that is defined by the
+        /// <see cref="T:xyLOGIX.Core.Debug.DebugLevel" /> enumeration.
+        /// </summary>
+        /// <param name="level">
+        /// (Required.) One of the
+        /// <see cref="T:xyLOGIX.Core.Debug.DebugLevel" /> value(s) that is to be examined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the debug <paramref name="level" /> falls
+        /// within the defined value set; <see langword="false" /> otherwise.
+        /// </returns>
+        bool IsValid(DebugLevel level);
+    }
+}
