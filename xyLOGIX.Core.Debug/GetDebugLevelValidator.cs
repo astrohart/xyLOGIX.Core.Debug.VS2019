1efc2e0635750fa37dc87cd2c059268af909ef3d
Brian Hart <brian.hart@xylogix.net>
2025-05-11 19:05:50 -0600

    Create GetDebugLevelValidator.cs
    - Introduce a new static class `GetDebugLevelValidator`
      - Located in the `xyLOGIX.Core.Debug` namespace
      - Provides singleton access to `IDebugLevelValidator`
    
    - Implement a static constructor
      - Automatically called before instance creation or static member access
    
    - Add `SoleInstance` method
      - Retrieves the singleton instance of `DebugLevelValidator`
      - Logs exceptions to Debug output and returns default value if needed
    
    - Include necessary using directives
      - `PostSharp.Patterns.Diagnostics`
      - `System`
      - `System.Diagnostics`

---
+++ xyLOGIX.Core.Debug/GetDebugLevelValidator.cs
@@ -0,0 +1,55 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:xyLOGIX.Core.Debug.IDebugLevelValidator" /> interface.
+    /// </summary>
+    public static class GetDebugLevelValidator
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:xyLOGIX.Core.Debug.GetDebugLevelValidator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetDebugLevelValidator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IDebugLevelValidator" /> interface, and returns
+        /// a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IDebugLevelValidator" /> interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        public static IDebugLevelValidator SoleInstance()
+        {
+            IDebugLevelValidator result;
+
+            try
+            {
+                result = DebugLevelValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the Debug output
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
