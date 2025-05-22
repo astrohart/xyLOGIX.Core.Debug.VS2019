c10c29e3d9d0ff7f3a6ae2a04f1541a5ecc8bb5c
Brian Hart <brian.hart@xylogix.net>
2025-05-22 16:43:05 -0600

    Update IAppenderFactory.cs
    - Added using directives for log4net.Appender and PostSharp.Patterns.Diagnostics.
    - Introduced new method GetAppender to the IAppenderFactory interface.
    - Updated documentation comments for properties and methods for clarity.

--- xyLOGIX.Core.Debug/IAppenderFactory.cs
+++ xyLOGIX.Core.Debug/IAppenderFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using log4net.Appender;
+using PostSharp.Patterns.Diagnostics;
+using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
@@ -13,9 +15,28 @@ namespace xyLOGIX.Core.Debug
     public interface IAppenderFactory
     {
         /// <summary>
-        /// Gets or sets one of the
-        /// <see cref="T:xyLOGIX.Core.Debug.AppenderRetrievalMode" /> enumeration values.
+        /// Gets the <see cref="T:xyLOGIX.Core.Debug.AppenderRetrievalMode" /> enumeration
+        /// value that identifies how this <c>Appender Factory</c> operates.
         /// </summary>
         AppenderRetrievalMode Mode { [DebuggerStepThrough] get; }
+
+        /// <summary>
+        /// Given the specified <paramref name="config" />, creates or retrieves an
+        /// <c>Appender</c> associated with the specified settings.
+        /// </summary>
+        /// <param name="config">
+        /// (Required.) Reference to an instance of an object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IRollingFileAppenderConfiguration" />
+        /// interface.
+        /// </param>
+        /// <returns>
+        /// If successful, a reference to an instance of an object that implements
+        /// the <see cref="T:log4net.Appender.IAppender" /> interface; otherwise, a
+        /// <see langword="null" /> reference is returned.
+        /// </returns>
+        [return: NotLogged]
+        IAppender GetAppender(
+            [NotLogged] IRollingFileAppenderConfiguration config
+        );
     }
 }
