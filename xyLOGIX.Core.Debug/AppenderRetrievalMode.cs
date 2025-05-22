cc26fa4cd22eb036aa6a75b887faedbb3b43a5b8
Brian Hart <brian.hart@xylogix.net>
2025-05-22 13:56:33 -0600

    Create AppenderRetrievalMode.cs
    - Added a new enumeration `AppenderRetrievalMode` to
      `AppenderRetrievalMode.cs` with the following values:
      - `CreateNew`: for creating a new appender
      - `GetExisting`: for obtaining an appender from the manager
      - `Unknown`: for an undefined retrieval mode
    
    - Included `AppenderRetrievalMode.cs` in the project file
      `xyLOGIX.Core.Debug.csproj` to ensure it is compiled.

---
+++ xyLOGIX.Core.Debug/AppenderRetrievalMode.cs
@@ -0,0 +1,26 @@
+﻿namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Value(s) that explain by which mode an <c>Appender</c> is to be retrieved,
+    /// whether a new <c>Appender</c> is to be created or whether an existing
+    /// <c>Appender</c> is to be obtained from the <c>Appender Manager</c>, for
+    /// example.
+    /// </summary>
+    public enum AppenderRetrievalMode
+    {
+        /// <summary>
+        /// Create a new <c>RollingFileAppender</c>.
+        /// </summary>
+        CreateNew,
+
+        /// <summary>
+        /// Attempt to obtain an <c>Appender</c> from the <c>Appender Manager</c>.
+        /// </summary>
+        GetExisting,
+
+        /// <summary>
+        /// Unknown retrieval mode.
+        /// </summary>
+        Unknown = -1
+    }
+}
