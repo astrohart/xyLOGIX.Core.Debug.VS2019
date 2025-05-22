0f07e0fa398f4392081a5d9736a3bdb7fdc5b8d9
Brian Hart <brian.hart@xylogix.net>
2025-05-22 16:05:16 -0600

    Create IAppenderFactory.cs
    - Introduce a new interface `IAppenderFactory` in the `xyLOGIX.Core.Debug` namespace.
    - Define a contract for creating or retrieving `Appender` objects for log message routing.
    - Include a `Mode` property to get or set an `AppenderRetrievalMode` enumeration value.
    - Apply `DebuggerStepThrough` attribute to the getter for improved debugging.

---
+++ xyLOGIX.Core.Debug/IAppenderFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Interface for factories that create or retrieve <c>Appender</c> objects.
+    /// </summary>
+    /// <remarks>
+    /// This interface is used to define the contract for any class that is responsible
+    /// for creating or retrieving <c>Appender</c>(s) for the purpose of routing log
+    /// messages to a specific destination(s).
+    /// </remarks>
+    public interface IAppenderFactory
+    {
+        /// <summary>
+        /// Gets or sets one of the
+        /// <see cref="T:xyLOGIX.Core.Debug.AppenderRetrievalMode" /> enumeration values.
+        /// </summary>
+        AppenderRetrievalMode Mode { [DebuggerStepThrough] get; }
+    }
+}
