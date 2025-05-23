0b968e0b0ce5960281122d5b2649d1c7a31d25e5
Brian Hart <brian.hart@xylogix.net>
2025-05-23 12:00:05 -0600

    Create GetAppenderRetrievalModeValidator.cs
    - Introduce a new static class `GetAppenderRetrievalModeValidator` in the `xyLOGIX.Core.Debug` namespace.
    - Provide a singleton instance of `IAppenderRetrievalModeValidator`.
    - Implement a static constructor for initialization.
    - Add a method `SoleInstance` to retrieve the singleton instance.
    - Include error handling to log exceptions during instance retrieval.

---
+++ xyLOGIX.Core.Debug/GetAppenderRetrievalModeValidator.cs
@@ -0,0 +1,55 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:xyLOGIX.Core.Debug.IAppenderRetrievalModeValidator" /> interface.
+    /// </summary>
+    public static class GetAppenderRetrievalModeValidator
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.GetAppenderRetrievalModeValidator" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetAppenderRetrievalModeValidator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IAppenderRetrievalModeValidator" /> interface,
+        /// and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IAppenderRetrievalModeValidator" /> interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        public static IAppenderRetrievalModeValidator SoleInstance()
+        {
+            IAppenderRetrievalModeValidator result;
+
+            try
+            {
+                result = AppenderRetrievalModeValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
