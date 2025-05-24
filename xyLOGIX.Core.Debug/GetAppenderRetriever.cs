3c2bf542a7d7b8a32031333bafa9c2f8c46e705a
Brian Hart <brian.hart@xylogix.net>
2025-05-24 02:57:20 -0600

    Create GetAppenderRetriever.cs
    - Introduce a new static class `GetAppenderRetriever` in the `xyLOGIX.Core.Debug` namespace.
    - Implement the `For` method to retrieve `IAppenderRetriever` instances based on `AppenderRetrievalMode`.
    - Include detailed XML documentation for the method, parameters, return value, and exceptions.
    - Support `CreateNew` mode with delegation to `GetCreateNewAppenderRetriever.SoleInstance()`.
    - Throw `ArgumentOutOfRangeException` for unsupported modes.

---
+++ xyLOGIX.Core.Debug/GetAppenderRetriever.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>Obtains references to instances of objects that implement the <see cref="T:xyLOGIX.Core.Debug.IAppenderRetriever" /> interface that change depending on the strategy desired.</summary>
+    public static class GetAppenderRetriever
+    {
+
+        /// <summary>Obtains a reference to an instance of an object that implements the <see cref="T:xyLOGIX.Core.Debug.IAppenderRetriever" /> interface which corresponds to the specified meeting <paramref name="mode" />.</summary>
+        /// <param name="mode"> (Required.) One of the <see cref="T:xyLOGIX.Core.Debug.AppenderRetrievalMode" /> enumeration values that describes the desired appender retrieval mode.</param>
+        /// <returns>Reference to the instance of the object that implements the <see cref="T:xyLOGIX.Core.Debug.IAppenderRetriever" /> interface which corresponds to the specific enumeration value that is specified for the argument of the <paramref name="mode" /> parameter.</returns>
+        /// <remarks>This method will throw an exception if there are no types implemented that correspond to the enumeration value passed for the argument of the <paramref name="mode" /> parameter.</remarks>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">Thrown if there is no corresponding concrete type defined that implements the <see cref="T:xyLOGIX.Core.Debug.IAppenderRetriever" /> interface and which corresponds to the specific enumeration value that was passed for the argument of the <paramref name="mode" /> parameter, if it is not supported.</exception>
+        public static xyLOGIX.Core.Debug.IAppenderRetriever For(xyLOGIX.Core.Debug.AppenderRetrievalMode mode)
+        {
+            xyLOGIX.Core.Debug.IAppenderRetriever result;
+
+            switch (mode)
+            {
+                case AppenderRetrievalMode.CreateNew:
+                    result = GetCreateNewAppenderRetriever.SoleInstance();
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(
+                        nameof(mode), mode, $"The specified Appender Retrieval Mode, '{mode}', is not supported."
+                    );
+            }
+
+            return result;
+        }
+    }
+}
