a2f0a07e175855dcb979f20ff6eb423ec2052253
Brian Hart <brian.hart@xylogix.net>
2025-05-24 02:16:15 -0600

    Update CreateNewAppenderRetriever.cs
    - Added validation for Rolling File Appender configuration.
    - Implemented logging for configuration validation results.
    - Enhanced debug statements for better traceability.

--- xyLOGIX.Core.Debug/CreateNewAppenderRetriever.cs
+++ xyLOGIX.Core.Debug/CreateNewAppenderRetriever.cs
@@ -1,4 +1,5 @@
 ﻿using log4net.Appender;
+using log4net.Layout;
 using PostSharp.Patterns.Diagnostics;
 using System;
 using System.Diagnostics;
@@ -27,7 +28,8 @@ namespace xyLOGIX.Core.Debug
         /// Gets a reference to the one and only instance of the object that implements the
         /// <see cref="T:xyLOGIX.Core.Debug.IAppenderRetriever" /> interface.
         /// </summary>
-        public static IAppenderRetriever Instance { [DebuggerStepThrough] get; } =
+        public static IAppenderRetriever
+            Instance { [DebuggerStepThrough] get; } =
             new CreateNewAppenderRetriever();
 
         /// <summary>
@@ -65,7 +67,7 @@ namespace xyLOGIX.Core.Debug
                     "CreateNewAppenderRetriever.GetFileAppenderByPath: Checking whether the 'config' method parameter has a null reference for a value..."
                 );
 
-                // Check to see if the required parameter, config, is null. If it is, send an 
+                // Check to see if the required parameter, config, is null. If it is, send an
                 // error to the Debug output and quit, returning the default return value of
                 // this method.
                 if (config == null)
@@ -87,7 +89,82 @@ namespace xyLOGIX.Core.Debug
                     "CreateNewAppenderRetriever.GetFileAppenderByPath: *** SUCCESS *** We have been passed a valid object reference for the 'config' method parameter.  Proceeding..."
                 );
 
+                System.Diagnostics.Debug.WriteLine(
+                    "CreateNewAppenderRetriever.GetAppender: Checking whether the Rolling File Appender Configuration has valid settings..."
+                );
 
+                // Check to see whether the Rolling File Appender Configuration has valid settings.
+                // If this is not the case, then write an error message to the log file,
+                // and then terminate the execution of this method.
+                if (!RollingFileAppenderConfigurationValidator.IsValid(config))
+                {
+                    // The Rolling File Appender Configuration does NOT have valid settings.  This is not desirable.
+                    System.Diagnostics.Debug.WriteLine(
+                        "CreateNewAppenderRetriever.GetAppender: *** ERROR *** The Rolling File Appender Configuration does NOT have valid settings.  Stopping..."
+                    );
+
+                    System.Diagnostics.Debug.WriteLine(
+                        $"*** CreateNewAppenderRetriever.GetAppender: Result = {result}"
+                    );
+
+                    // stop.
+                    return result;
+                }
+
+                System.Diagnostics.Debug.WriteLine(
+                    "CreateNewAppenderRetriever.GetAppender: *** SUCCESS *** The Rolling File Appender Configuration has valid settings.  Getting a new 'RollingFileAppender'..."
+                );
+
+                var rollerToUse =
+                    MakeNewRollingFileAppender.ForRollingStyle(
+                        config.RollingStyle
+                    );
+
+                System.Diagnostics.Debug.WriteLine(
+                    "CreateNewAppenderRetriever.GetAppender: Checking whether the variable, 'rollerToUse', has a null reference for a value..."
+                );
+
+                // Check to see if the variable, 'rollerToUse', has a null reference for a value.
+                // If it does, then emit an error to the Debug output, and terminate the execution
+                // of this method, returning the default return value.
+                if (rollerToUse == null)
+                {
+                    // The variable, 'rollerToUse', has a null reference for a value.  This is not desirable.
+                    System.Diagnostics.Debug.WriteLine(
+                        "CreateNewAppenderRetriever.GetAppender: *** ERROR ***  The variable, 'rollerToUse', has a null reference for a value.  Stopping..."
+                    );
+
+                    System.Diagnostics.Debug.WriteLine(
+                        $"*** CreateNewAppenderRetriever.GetAppender: Result = {result}"
+                    );
+
+                    // stop.
+                    return result;
+                }
+
+                // We can use the variable, 'rollerToUse', because it's not set to a null reference.
+                System.Diagnostics.Debug.WriteLine(
+                    "CreateNewAppenderRetriever.GetAppender: *** SUCCESS *** The variable, 'rollerToUse', has a valid object reference for its value.  Proceeding..."
+                );
+
+                System.Diagnostics.Debug.WriteLine(
+                    $"*** FYI *** Configuring the 'RollingFileAppender' with the following settings: {Environment.NewLine}\tFile = {config.File}{Environment.NewLine}\tLayout = {config.Layout}{Environment.NewLine}\tMaxSizeRollBackups = {config.MaxSizeRollBackups}{Environment.NewLine}\tMaximumFileSize = {config.MaximumFileSize}{Environment.NewLine}\tAppendToFile = {config.AppendToFile}{Environment.NewLine}\tStaticLogFileName = {config.StaticLogFileName}"
+                );
+
+                result = rollerToUse.SetLogFileNameTo(config.File)
+                                    .WithPatternLayout(
+                                        config.Layout as PatternLayout
+                                    )
+                                    .AndMaximumNumberOfRollingBackups(
+                                        config.MaxSizeRollBackups
+                                    )
+                                    .WithMaximumFileSizeOf(
+                                        config.MaximumFileSize
+                                    )
+                                    .ThatShouldAppendToFile(config.AppendToFile)
+                                    .AndThatHasAStaticLogFileName(
+                                        config.StaticLogFileName
+                                    );
             }
             catch (Exception ex)
             {
