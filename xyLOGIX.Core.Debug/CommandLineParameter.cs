2679661b8700e553cd9fe0df8f9249accf7e7075
Brian Hart <brian.hart@xylogix.net>
2025-05-27 11:41:56 -0600

    Updating CommandLineParameter.cs
    - Change visibility of `CommandLineParameter` class from `public static` to `internal static`.
    - Remove the static constructor, eliminating any initialization logic that was previously executed.

--- xyLOGIX.Core.Debug/CommandLineParameter.cs
+++ xyLOGIX.Core.Debug/CommandLineParameter.cs
@@ -5,7 +5,8 @@ namespace xyLOGIX.Core.Debug
     /// <summary>
     /// Values for command-line parameters.
     /// </summary>
-    public static class CommandLineParameter
+    [Log(AttributeExclude = true)]
+    internal static class CommandLineParameter
     {
         /// <summary>
         /// A <see cref="T:System.String" /> that contains the text of the
@@ -14,19 +15,5 @@ namespace xyLOGIX.Core.Debug
         /// method when an exception is thrown.
         /// </summary>
         public const string HaltOnException = "--hoe";
-
-        /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.CommandLineParameter" /> class.
-        /// </summary>
-        /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
-        /// <para />
-        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
-        /// attribute in order to simplify the logging output.
-        /// </remarks>
-        [Log(AttributeExclude = true)]
-        static CommandLineParameter() { }
     }
 }
