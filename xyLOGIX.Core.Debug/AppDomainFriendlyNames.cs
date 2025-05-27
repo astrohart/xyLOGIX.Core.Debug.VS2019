bcc8704b4e975382f7ae1b4947afe668f954fe83
Brian Hart <brian.hart@xylogix.net>
2025-05-27 11:43:16 -0600

    Update AppDomainFriendlyNames.cs
    - Change `AppDomainFriendlyNames` class from `public` to `internal`
    - Add `Log(AttributeExclude = true)` attribute to the class
    - Remove static constructor and its documentation comments

--- xyLOGIX.Core.Debug/AppDomainFriendlyNames.cs
+++ xyLOGIX.Core.Debug/AppDomainFriendlyNames.cs
@@ -6,26 +6,13 @@ namespace xyLOGIX.Core.Debug
     /// Exposes <see cref="T:System.String" /> constants that contain
     /// standardized friendly names for application domains.
     /// </summary>
-    public static class AppDomainFriendlyNames
+    [Log(AttributeExclude = true)]
+    internal static class AppDomainFriendlyNames
     {
         /// <summary>
         /// A <see cref="T:System.String" /> that contains the friendly name of
         /// the <c>AppDomain</c> for LINQPad.
         /// </summary>
         public const string LINQPad = "LINQPad";
-
-        /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.AppDomainFriendlyNames" /> class.
-        /// </summary>
-        /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
-        /// <para />
-        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
-        /// attribute in order to simplify the logging output.
-        /// </remarks>
-        [Log(AttributeExclude = true)]
-        static AppDomainFriendlyNames() { }
     }
 }
