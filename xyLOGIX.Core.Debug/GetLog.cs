602199b2d6f9a68c0d506e2eebda69951c6f2eae
Brian Hart <brian.hart@xylogix.net>
2025-05-27 11:21:50 -0600

    Update GetLog.cs
    - Changed access modifier for `GetLog` class from `public` to `internal`.
    - Changed access modifier for `FileName` field from `public` to `internal`.
    - Changed access modifier for `FileFolder` property from `public` to `internal`.
    - Changed access modifier for `FilePath` property from `public` to `internal`.

--- xyLOGIX.Core.Debug/GetLog.cs
+++ xyLOGIX.Core.Debug/GetLog.cs
@@ -6,10 +6,10 @@ using System.Diagnostics;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary> Gets data used for logging. </summary>
-    public static class GetLog
+    internal static class GetLog
     {
         /// <summary> String containing the pattern to use for the name of the log file. </summary>
-        public static readonly string FileName =
+        internal static readonly string FileName =
             $"dd_{SetLog.ApplicationName}_Run_{Guid.NewGuid():N}_{DateTime.Now:yyyyMMMddHHmmss}_log.txt";
 
         /// <summary>
@@ -27,7 +27,7 @@ namespace xyLOGIX.Core.Debug
         /// Gets a string that contains the fully-qualified pathname of the
         /// current user's temporary files folder.
         /// </summary>
-        public static string FileFolder
+        internal static string FileFolder
         {
             [DebuggerStepThrough]
             get
@@ -54,7 +54,7 @@ namespace xyLOGIX.Core.Debug
         /// Gets a string containing the fully-qualified pathname to use for the
         /// current log file.
         /// </summary>
-        public static string FilePath
+        internal static string FilePath
         {
             [DebuggerStepThrough]
             get
