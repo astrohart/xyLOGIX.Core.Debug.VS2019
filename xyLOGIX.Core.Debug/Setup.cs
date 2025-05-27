eeaec7ec91f6528fc2bbebe79864bdb92ec60afb
Brian Hart <brian.hart@xylogix.net>
2025-05-26 19:09:09 -0600

    Update Setup.cs
    - Change `Setup` class from public to internal.
    - Add `DetermineEventSourceName` method for application name retrieval.
    - Modify `EventLogging` method accessibility from public to internal.
    - Update method signatures and enhance documentation for clarity.

--- xyLOGIX.Core.Debug/Setup.cs
+++ xyLOGIX.Core.Debug/Setup.cs
@@ -5,31 +5,89 @@ using System.Diagnostics;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary> Exposes <see langword="static" /> methods to perform setup tasks. </summary>
-    public static class Setup
+    [Log(AttributeExclude = true)]
+    internal static class Setup
     {
-        /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.Setup" /> class.
-        /// </summary>
-        /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
-        /// <para />
-        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
-        /// attribute in order to simplify the logging output.
-        /// </remarks>
-        [Log(AttributeExclude = true)]
-        static Setup() { }
-
         /// <summary>
         /// Gets a reference to an instance of an object that implements the
         /// <see cref="T:xyLOGIX.Core.Debug.IEventLogManager" /> interface.
         /// </summary>
         private static IEventLogManager EventLogManager
         {
-            [DebuggerStepThrough]
-            get;
+            [DebuggerStepThrough] get;
         } = GetEventLogManager.SoleInstance();
+
+        /// <summary>
+        /// Attempts to determine the proper value for the
+        /// <see cref="P:xyLOGIX.Core.Debug.DebugUtils.ApplicationName" /> property, given
+        /// the specified <paramref name="applicationName" />.
+        /// </summary>
+        /// <param name="applicationName">
+        /// (Optional.) A <see cref="T:System.String" /> that contains the application name
+        /// that is to be used; if this value is blank, <see langword="null" />, or the
+        /// <see cref="F:System.String.Empty" /> value, then the result of attempting to
+        /// get the assembly <c>ProductName</c> value is used instead.
+        /// <para />
+        /// The default value of this parameter is the <see cref="F:System.String.Empty" />
+        /// value.
+        /// </param>
+        /// <returns>
+        /// If successful, a <see cref="T:System.String" /> that contains the
+        /// value that is to be used for the
+        /// <see cref="P:xyLOGIX.Core.Debug.DebugUtils.ApplicationName" /> property;
+        /// otherwise, the <see cref="F:System.String.Empty" /> value is returned.
+        /// </returns>
+        [return: NotLogged]
+        private static string DetermineEventSourceName(
+            [NotLogged] string applicationName = ""
+        )
+        {
+            var result = GetEvent.SourceName();
+
+            try
+            {
+                System.Diagnostics.Debug.WriteLine(
+                    "Setup.DetermineEventSourceName *** INFO: Checking whether the value of the parameter, 'applicationName', is blank..."
+                );
+
+                // Check whether the value of the parameter, 'applicationName', is blank.
+                // If this is so, then emit an error message to the log file, and
+                // then terminate the execution of this method.
+                if (string.IsNullOrWhiteSpace(applicationName))
+                {
+                    // The parameter, 'applicationName' was either passed a null value, or it is blank.  This is not desirable.
+                    System.Diagnostics.Debug.WriteLine(
+                        "Setup.DetermineEventSourceName: The parameter, 'applicationName' was either passed a null value, or it is blank. Stopping..."
+                    );
+
+                    System.Diagnostics.Debug.WriteLine(
+                        $"Setup.DetermineEventSourceName: Result = '{result}'"
+                    );
+
+                    // stop.
+                    return result;
+                }
+
+                System.Diagnostics.Debug.WriteLine(
+                    "*** SUCCESS *** The parameter 'applicationName' is not blank.  Proceeding..."
+                );
+
+                result = applicationName;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the Debug output window
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = string.Empty;
+            }
+
+            System.Diagnostics.Debug.WriteLine(
+                $"Setup.DetermineEventSourceName: Result = '{result}'"
+            );
+
+            return result;
+        }
 
         /// <summary>
         /// Sets up the Windows Event Log Application log quote to correspond
@@ -46,7 +104,9 @@ namespace xyLOGIX.Core.Debug
         /// <see langword="false" /> otherwise.
         /// </returns>
         [DebuggerStepThrough]
-        public static bool EventLogging([NotLogged] string applicationName = "")
+        internal static bool EventLogging(
+            [NotLogged] string applicationName = ""
+        )
         {
             var result = false;
 
@@ -174,78 +234,6 @@ namespace xyLOGIX.Core.Debug
 
             System.Diagnostics.Debug.WriteLine(
                 $"Setup.EventLogging: Result = {result}"
-            );
-
-            return result;
-        }
-
-        /// <summary>
-        /// Attempts to determine the proper value for the
-        /// <see cref="P:xyLOGIX.Core.Debug.DebugUtils.ApplicationName" /> property, given
-        /// the specified <paramref name="applicationName" />.
-        /// </summary>
-        /// <param name="applicationName">
-        /// (Optional.) A <see cref="T:System.String" /> that contains the application name
-        /// that is to be used; if this value is blank, <see langword="null" />, or the
-        /// <see cref="F:System.String.Empty" /> value, then the result of attempting to
-        /// get the assembly <c>ProductName</c> value is used instead.
-        /// <para />
-        /// The default value of this parameter is the <see cref="F:System.String.Empty" />
-        /// value.
-        /// </param>
-        /// <returns>
-        /// If successful, a <see cref="T:System.String" /> that contains the
-        /// value that is to be used for the
-        /// <see cref="P:xyLOGIX.Core.Debug.DebugUtils.ApplicationName" /> property;
-        /// otherwise, the <see cref="F:System.String.Empty" /> value is returned.
-        /// </returns>
-        [return: NotLogged]
-        private static string DetermineEventSourceName(
-            [NotLogged] string applicationName = ""
-        )
-        {
-            var result = GetEvent.SourceName();
-
-            try
-            {
-                System.Diagnostics.Debug.WriteLine(
-                    "Setup.DetermineEventSourceName *** INFO: Checking whether the value of the parameter, 'applicationName', is blank..."
-                );
-
-                // Check whether the value of the parameter, 'applicationName', is blank.
-                // If this is so, then emit an error message to the log file, and
-                // then terminate the execution of this method.
-                if (string.IsNullOrWhiteSpace(applicationName))
-                {
-                    // The parameter, 'applicationName' was either passed a null value, or it is blank.  This is not desirable.
-                    System.Diagnostics.Debug.WriteLine(
-                        "Setup.DetermineEventSourceName: The parameter, 'applicationName' was either passed a null value, or it is blank. Stopping..."
-                    );
-
-                    System.Diagnostics.Debug.WriteLine(
-                        $"Setup.DetermineEventSourceName: Result = '{result}'"
-                    );
-
-                    // stop.
-                    return result;
-                }
-
-                System.Diagnostics.Debug.WriteLine(
-                    "*** SUCCESS *** The parameter 'applicationName' is not blank.  Proceeding..."
-                );
-
-                result = applicationName;
-            }
-            catch (Exception ex)
-            {
-                // dump all the exception info to the Debug output window
-                System.Diagnostics.Debug.WriteLine(ex);
-
-                result = string.Empty;
-            }
-
-            System.Diagnostics.Debug.WriteLine(
-                $"Setup.DetermineEventSourceName: Result = '{result}'"
             );
 
             return result;
