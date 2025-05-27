1115df4df69a39856a1ea6b7192b399160bdaaa0
Brian Hart <brian.hart@xylogix.net>
2025-05-27 11:26:56 -0600

    Update GetAppenderManager.cs
    - Removed static constructor and related XML comments.
    - Added XML comment to exclude class from logging attributes.

--- xyLOGIX.Core.Debug/GetAppenderManager.cs
+++ xyLOGIX.Core.Debug/GetAppenderManager.cs
@@ -8,19 +8,9 @@ namespace xyLOGIX.Core.Debug
     /// Provides access to the one and only instance of the object that implements the
     /// <see cref="T:xyLOGIX.Core.Debug.IAppenderManager" /> interface.
     /// </summary>
+    [Log(AttributeExclude = true)]
     public static class GetAppenderManager
     {
-        /// <summary>
-        /// Initializes static data or performs actions that need to be performed
-        /// once only for the <see cref="T:xyLOGIX.Core.Debug.GetAppenderManager" /> class.
-        /// </summary>
-        /// <remarks>
-        /// This constructor is called automatically prior to the first instance
-        /// being created or before any static members are referenced.
-        /// </remarks>
-        [Log(AttributeExclude = true)]
-        static GetAppenderManager() { }
-
         /// <summary>
         /// Obtains access to the sole instance of the object that implements the
         /// <see cref="T:xyLOGIX.Core.Debug.IAppenderManager" /> interface, and returns a
