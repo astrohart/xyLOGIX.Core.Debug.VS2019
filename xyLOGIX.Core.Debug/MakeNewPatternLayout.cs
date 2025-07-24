d7ce5869baeaa12dcd7395f1519a53e5ced0e8af
Brian Hart <brian.hart@xylogix.net>
2025-07-24 10:08:57 -0600

    Update MakeNewPatternLayout.cs
    - Removed the static constructor from the MakeNewPatternLayout class.
    - Retained and moved the [Log(AttributeExclude = true)] attribute to the class level.
    - Eliminated associated XML documentation comments for the static constructor.

--- xyLOGIX.Core.Debug/MakeNewPatternLayout.cs
+++ xyLOGIX.Core.Debug/MakeNewPatternLayout.cs
@@ -9,22 +9,9 @@ namespace xyLOGIX.Core.Debug
     /// <see cref="T:log4net.Layout.PatternLayout" /> that are initialized properly,
     /// and return a reference to them.
     /// </summary>
+    [Log(AttributeExclude = true)]
     internal static class MakeNewPatternLayout
     {
-        /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.MakeNewPatternLayout" /> class.
-        /// </summary>
-        /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
-        /// <para />
-        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
-        /// attribute in order to simplify the logging output.
-        /// </remarks>
-        [Log(AttributeExclude = true)]
-        static MakeNewPatternLayout() { }
-
         /// <summary>
         /// Creates a new instance of <see cref="T:log4net.Layout.PatternLayout" /> that is
         /// initialized with the specified <paramref name="conversionPattern" />, and
