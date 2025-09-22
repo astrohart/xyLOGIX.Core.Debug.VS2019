4b46bf9c92ec1d6fc23992ee1af7dcc92913c364
Brian Hart <brian.hart@xylogix.net>
2025-09-21 18:15:14 -0600

    Update MuteConsoleChangedEventHandler.cs
    - Replacing all occurrences of `object sender` and `EventArgs e`, respectively, are prefaced with `[NotLogged]`.  This is being done so as to ensure that the value(s) of these parameter(s) do not get written to PostSharp log file(s).  Also updated all auto-property(ies) to make sure their getters and setters are all prefixed with the `[DebuggerStepThrough]` attribute.

--- xyLOGIX.Core.Debug/MuteConsoleChangedEventHandler.cs
+++ xyLOGIX.Core.Debug/MuteConsoleChangedEventHandler.cs
@@ -18,6 +18,6 @@ namespace xyLOGIX.Core.Debug
     /// </remarks>
     public delegate void MuteConsoleChangedEventHandler(
         [NotLogged] object sender,
-        [NotLogged] MuteConsoleChangedEventArgs e
+        [NotLogged] MuteConsoleChanged[NotLogged] EventArgs e
     );
 }
