e0708180beae977ef8cb33638e8b36b0b0e2c04f
Brian Hart <brian.hart@xylogix.net>
2025-11-08 13:44:57 -0700

    Update FromConfigFileLoggingConfigurator.cs
    - Removed all reference(s) to the `xyLOGIX.Files.Action` module, as it is being retired.  Also, reformatted the source file(s) in accordance with rules that are defined in CodeMaid and ReSharper.

--- xyLOGIX.Core.Debug/FromConfigFileLoggingConfigurator.cs
+++ xyLOGIX.Core.Debug/FromConfigFileLoggingConfigurator.cs
@@ -14,12 +14,14 @@ namespace xyLOGIX.Core.Debug
     internal class FromConfigFileLoggingConfigurator : LoggingConfiguratorBase
     {
         /// <summary>
-        /// Empty, <see langword="static" /> constructor to prohibit direct allocation of this class.
+        /// Empty, <see langword="static" /> constructor to prohibit direct allocation of
+        /// this class.
         /// </summary>
         static FromConfigFileLoggingConfigurator() { }
 
         /// <summary>
-        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of this class.
+        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of
+        /// this class.
         /// </summary>
         [Log(AttributeExclude = true)]
         private FromConfigFileLoggingConfigurator() { }
