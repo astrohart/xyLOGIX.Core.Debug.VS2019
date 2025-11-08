88ad1f09a718bbca23b86dae44ad4981e7dd396a
Brian Hart <brian.hart@xylogix.net>
2025-11-08 13:45:04 -0700

    Update Initialize.cs
    - Removed all reference(s) to the `xyLOGIX.Files.Action` module, as it is being retired.  Also, reformatted the source file(s) in accordance with rules that are defined in CodeMaid and ReSharper.

--- xyLOGIX.Core.Debug/Initialize.cs
+++ xyLOGIX.Core.Debug/Initialize.cs
@@ -10,10 +10,12 @@ namespace xyLOGIX.Core.Debug
     internal static class Initialize
     {
         /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only for the <see cref="T:xyLOGIX.Core.Debug.Initialize"/> class.
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the <see cref="T:xyLOGIX.Core.Debug.Initialize" /> class.
         /// </summary>
         /// <remarks>
-        /// This constructor is called automatically prior to the first instance being created or before any static members are referenced.
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
         /// <para />
         /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
         /// attribute in order to simplify the logging output.
