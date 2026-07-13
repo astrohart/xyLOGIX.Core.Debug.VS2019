daab47ac8d4e39073ffcb88e3bf43bfe89d7ff9d
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:44:41 -0600

    Update GetOutputLocationProvider.cs
    - Reformatted XML documentation comments for improved readability.
    - Rewrapped summary and return-value documentation for consistent formatting.
    - Preserved the existing behavior and public API of `GetOutputLocationProvider`.

--- xyLOGIX.Core.Debug/GetOutputLocationProvider.cs
+++ xyLOGIX.Core.Debug/GetOutputLocationProvider.cs
@@ -12,12 +12,13 @@ namespace xyLOGIX.Core.Debug
     internal static class GetOutputLocationProvider
     {
         /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.GetOutputLocationProvider" /> class.
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:xyLOGIX.Core.Debug.GetOutputLocationProvider" />
+        /// class.
         /// </summary>
         /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
         /// </remarks>
         [Log(AttributeExclude = true)]
         static GetOutputLocationProvider() { }
@@ -25,13 +26,11 @@ namespace xyLOGIX.Core.Debug
         /// <summary>
         /// Obtains access to the sole instance of the object that implements the
         /// <see cref="T:xyLOGIX.Core.Debug.IOutputLocationProvider" /> interface, and
-        /// returns a
-        /// reference to it.
+        /// returns a reference to it.
         /// </summary>
         /// <returns>
-        /// Reference to the one, and only, instance of the object that
-        /// implements the <see cref="T:xyLOGIX.Core.Debug.IOutputLocationProvider" />
-        /// interface.
+        /// Reference to the one, and only, instance of the object that implements
+        /// the <see cref="T:xyLOGIX.Core.Debug.IOutputLocationProvider" /> interface.
         /// </returns>
         [DebuggerStepThrough]
         [return: NotLogged]
