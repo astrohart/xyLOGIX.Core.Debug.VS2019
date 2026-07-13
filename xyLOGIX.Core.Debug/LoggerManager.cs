873184a283ab1eee00d5ddf302e1a5d73f54aaef
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:49:02 -0600

    Update LoggerManager.cs
    - Reformatted XML documentation comments for improved readability.
    - Consolidated property, method declaration, and helper method invocation formatting.
    - Rewrapped inline comments for consistent formatting.
    - Simplified parameter documentation without changing behavior.

--- xyLOGIX.Core.Debug/LoggerManager.cs
+++ xyLOGIX.Core.Debug/LoggerManager.cs
@@ -13,12 +13,12 @@ namespace xyLOGIX.Core.Debug
     internal static class LoggerManager
     {
         /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.LoggerManager" /> class.
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:xyLOGIX.Core.Debug.LoggerManager" /> class.
         /// </summary>
         /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
         /// <para />
         /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
         /// attribute in order to simplify the logging output.
@@ -33,45 +33,39 @@ namespace xyLOGIX.Core.Debug
         /// </summary>
         private static IRootLoggerProvisioningStrategyValidator
             RootLoggerProvisioningStrategyValidator
-        {
-            [DebuggerStepThrough] get;
-        } =
+        { [DebuggerStepThrough] get; } =
             GetRootLoggerProvisioningStrategyValidator.SoleInstance();
 
         /// <summary>
         /// Gets a reference to the default logger repository's root instance of
         /// <see cref="T:log4net.Hierarchy.Repository.Logger" />.
         /// </summary>
-        /// <param name="loggerRepository"> </param>
+        /// <param name="loggerRepository"></param>
         /// <returns>
         /// Reference to an instance of
         /// <see cref="T:log4net.Repository.Hierarchy.Logger" /> that refers to the
         /// <c>Root Logger</c> component that is to be used, if found; a
         /// <see langword="null" /> reference is returned otherwise.
         /// </returns>
-        internal static Logger GetRootLogger(
-            [NotLogged] ILoggerRepository loggerRepository = null
-        )
+        internal static Logger GetRootLogger([NotLogged] ILoggerRepository loggerRepository = null)
         {
             Logger result = default;
 
             try
             {
-                var strategy =
-                    Determine.TheRootLoggerProvisioningStrategyToUse(
-                        loggerRepository
-                    );
+                var strategy = Determine.TheRootLoggerProvisioningStrategyToUse(loggerRepository);
 
                 System.Diagnostics.Debug.WriteLine(
                     $"*** LoggerManager.GetRootLogger: Checking whether the Root Logger Provisioning Strategy, '{strategy}', is within the defined value set..."
                 );
 
-                // Check to see whether the Root Logger Provisioning Strategy is within the defined value set.
-                // If this is not the case, then write an error message to the log file,
+                // Check to see whether the Root Logger Provisioning Strategy is within the defined
+                // value set. If this is not the case, then write an error message to the log file,
                 // and then terminate the execution of this method.
                 if (!RootLoggerProvisioningStrategyValidator.IsValid(strategy))
                 {
-                    // The Root Logger Provisioning Strategy is NOT within the defined value set.  This is not desirable.
+                    // The Root Logger Provisioning Strategy is NOT within the defined value set.
+                    // This is not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         $"*** ERROR *** The Root Logger Provisioning Strategy, '{strategy}', is NOT within the defined value set.  Stopping..."
                     );
@@ -90,19 +84,19 @@ namespace xyLOGIX.Core.Debug
                     $"LoggerManager.GetRootLogger: Obtaining a reference to the Root Logger Provisioner for the Root Logger Provisioning Strategy, '{strategy}'..."
                 );
 
-                var rootLoggerProvisioner =
-                    GetRootLoggerProvisioner.For(strategy);
+                var rootLoggerProvisioner = GetRootLoggerProvisioner.For(strategy);
 
                 System.Diagnostics.Debug.WriteLine(
                     "LoggerManager.GetRootLogger: Checking whether the variable, 'rootLoggerProvisioner', has a null reference for a value..."
                 );
 
-                // Check to see if the variable, rootLoggerProvisioner, is null.  If it is, send an error
-                // to the log file, and then terminate the execution of this method,
-                // returning the default return value.
+                // Check to see if the variable, rootLoggerProvisioner, is null.  If it is, send an
+                // error to the log file, and then terminate the execution of this method, returning
+                // the default return value.
                 if (rootLoggerProvisioner == null)
                 {
-                    // the variable rootLoggerProvisioner is required to have a valid object reference.
+                    // the variable rootLoggerProvisioner is required to have a valid object
+                    // reference.
                     System.Diagnostics.Debug.WriteLine(
                         "LoggerManager.GetRootLogger: *** ERROR ***  The variable, 'rootLoggerProvisioner', has a null reference.  Stopping..."
                     );
@@ -111,7 +105,8 @@ namespace xyLOGIX.Core.Debug
                     return result;
                 }
 
-                // We can use the variable, rootLoggerProvisioner, because it's not set to a null reference.
+                // We can use the variable, rootLoggerProvisioner, because it's not set to a null
+                // reference.
                 System.Diagnostics.Debug.WriteLine(
                     "LoggerManager.GetRootLogger: *** SUCCESS *** The variable, 'rootLoggerProvisioner', has a valid object reference for its value.  Proceeding..."
                 );
