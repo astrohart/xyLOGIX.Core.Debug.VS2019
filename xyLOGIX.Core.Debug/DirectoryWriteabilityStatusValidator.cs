b5c2b3f075231a88bb56898e2b919ad5329f9e7c
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:31:56 -0600

    Update DirectoryWriteabilityStatusValidator.cs
    - Reflowed XML documentation, inline comments, and block comments for improved readability.
    - Reformatted singleton property, constructor, and class declarations for improved consistency.
    - Consolidated line wrapping throughout directory writeability status validation logic without changing behavior.

--- xyLOGIX.Core.Debug/DirectoryWriteabilityStatusValidator.cs
+++ xyLOGIX.Core.Debug/DirectoryWriteabilityStatusValidator.cs
@@ -5,13 +5,12 @@ using System.Diagnostics;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Validates whether certain value(s) are within the defined value set of the
-    /// <see cref="T:xyLOGIX.Core.Debug.DirectoryWriteabilityStatus" /> enumeration.
+    /// Validates whether certain value(s) are within the defined value set of
+    /// the <see cref="T:xyLOGIX.Core.Debug.DirectoryWriteabilityStatus" />
+    /// enumeration.
     /// </summary>
     [Log(AttributeExclude = true)]
-    internal class
-        DirectoryWriteabilityStatusValidator :
-        IDirectoryWriteabilityStatusValidator
+    internal class DirectoryWriteabilityStatusValidator : IDirectoryWriteabilityStatusValidator
     {
         /// <summary>
         /// Empty, <see langword="static" /> constructor to prohibit direct
@@ -20,12 +19,12 @@ namespace xyLOGIX.Core.Debug
         static DirectoryWriteabilityStatusValidator() { }
 
         /// <summary>
-        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of
-        /// this
-        /// class.
+        /// Empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
-        private DirectoryWriteabilityStatusValidator() { }
+        private DirectoryWriteabilityStatusValidator()
+        { }
 
         /// <summary>
         /// Gets a reference to the one and only instance of the object that implements the
@@ -35,7 +34,8 @@ namespace xyLOGIX.Core.Debug
         /// </summary>
         internal static IDirectoryWriteabilityStatusValidator Instance
         {
-            [DebuggerStepThrough] get;
+            [DebuggerStepThrough]
+            get;
         } = new DirectoryWriteabilityStatusValidator();
 
         /// <summary>
@@ -47,8 +47,7 @@ namespace xyLOGIX.Core.Debug
         /// <param name="status">
         /// (Required.) One of the
         /// <see cref="T:xyLOGIX.Core.Debug.DirectoryWriteabilityStatus" /> value(s) that
-        /// is
-        /// to be examined.
+        /// is to be examined.
         /// </param>
         /// <returns>
         /// <see langword="true" /> if the directory writeability
@@ -66,32 +65,26 @@ namespace xyLOGIX.Core.Debug
                     $"DirectoryWriteabilityStatusValidator.IsValid: status = '{status}'"
                 );
 
-                /*
-                 * For cybersecurity reasons, and to defeat reverse-engineering,
-                 * check the value of the 'status' parameter to ensure that it
-                 * is not set to a value outside the set of valid values defined
-                 * by the xyLOGIX.Core.Debug.DirectoryWriteabilityStatus
-                 * enumeration.
-                 *
-                 * In principle, since all C# enums devolve to integer values, a
-                 * hacker could insert a different value into the CPU register that the
-                 * 'status' parameter is read from and thereby make this application
-                 * do something it's not intended to do.
+                /* For cybersecurity reasons, and to defeat reverse-engineering, check the value of
+                 the 'status' parameter to ensure that it is not set to a value outside the set of
+                 valid values defined by the xyLOGIX.Core.Debug.DirectoryWriteabilityStatus
+                 enumeration. In principle, since all C# enums devolve to integer values, a hacker
+                 could insert a different value into the CPU register that the 'status' parameter is
+                 read from and thereby make this application do something it's not intended to do.
                  */
 
                 System.Diagnostics.Debug.WriteLine(
                     $"DirectoryWriteabilityStatusValidator.IsValid: Checking whether the value of the 'status' parameter, i.e., '{status}', is within the defined value set of its enumerated data type..."
                 );
 
-                // Check whether the value of the 'status' parameter is within the defined value set of its
-                // enumeration data type.  If this is not the case, then write an error message to the log
-                // file, and then terminate the execution of this method while returning the default return
-                // value.
-                if (!Enum.IsDefined(
-                        typeof(DirectoryWriteabilityStatus), status
-                    ))
+                // Check whether the value of the 'status' parameter is within the defined value set
+                // of its enumeration data type.  If this is not the case, then write an error
+                // message to the log file, and then terminate the execution of this method while
+                // returning the default return value.
+                if (!Enum.IsDefined(typeof(DirectoryWriteabilityStatus), status))
                 {
-                    // The value of the 'status' parameter is NOT within the defined value set for its enumerated data type.  This is not desirable.
+                    // The value of the 'status' parameter is NOT within the defined value set for
+                    // its enumerated data type.  This is not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         $"*** ERROR *** The value of the 'status' parameter, i.e., '{status}', is NOT within the defined value set of its enumerated data type.  Stopping..."
                     );
@@ -112,12 +105,14 @@ namespace xyLOGIX.Core.Debug
                     "DirectoryWriteabilityStatusValidator.IsValid: Checking whether the 'Unknown' value has NOT been specified for the 'status' parameter..."
                 );
 
-                // Check whether the 'Unknown' value has been specified for the 'status' parameter.  If this is the case, then
-                // write an error message to the log file, and then terminate the execution of this method, returning the default
-                // return value in order to indicate that this method failed.
+                // Check whether the 'Unknown' value has been specified for the 'status' parameter.
+                // If this is the case, then write an error message to the log file, and then
+                // terminate the execution of this method, returning the default return value in
+                // order to indicate that this method failed.
                 if (DirectoryWriteabilityStatus.Unknown.Equals(status))
                 {
-                    // The 'Unknown' value has been specified for the 'status' parameter.  This is not desirable.
+                    // The 'Unknown' value has been specified for the 'status' parameter.  This is
+                    // not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         "*** ERROR *** The 'Unknown' value has been specified for the 'status' parameter.  Stopping..."
                     );
@@ -134,9 +129,7 @@ namespace xyLOGIX.Core.Debug
                     "DirectoryWriteabilityStatusValidator.IsValid: *** SUCCESS *** The 'Unknown' value has NOT been specified for the 'status' parameter.  Proceeding..."
                 );
 
-                /*
-                 * If we made it here, then assume that the input data is valid.
-                 */
+                /* If we made it here, then assume that the input data is valid. */
 
                 result = true;
             }
