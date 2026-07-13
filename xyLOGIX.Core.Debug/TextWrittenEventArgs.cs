630ab3e1621a5eaea69039f236fd3b73cf4c7810
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:59:41 -0600

    Update TextWrittenEventArgs.cs
    - Reformatted XML documentation throughout `TextWrittenEventArgs` for improved consistency and readability.
    - Adjusted the static constructor documentation to use clearer line wrapping while preserving its content.
    - Simplified the XML documentation for the `Text` property by using the repository's preferred single-line summary style.

--- xyLOGIX.Core.Debug/TextWrittenEventArgs.cs
+++ xyLOGIX.Core.Debug/TextWrittenEventArgs.cs
@@ -5,17 +5,18 @@ using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
-    /// <summary> Provides information for <c>TextWritten</c> event handlers. </summary>
+    /// <summary>Provides information for <c>TextWritten</c> event handlers.</summary>
     [ExplicitlySynchronized]
     public class TextWrittenEventArgs : EventArgs
     {
         /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.TextWrittenEventArgs" /> class.
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:xyLOGIX.Core.Debug.TextWrittenEventArgs" />
+        /// class.
         /// </summary>
         /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
         /// <para />
         /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
         /// attribute in order to simplify the logging output.
@@ -33,7 +34,7 @@ namespace xyLOGIX.Core.Debug
         public TextWrittenEventArgs([NotLogged] string text)
             => Text = text;
 
-        /// <summary> Gets a string containing the text to be written. </summary>
+        /// <summary>Gets a string containing the text to be written.</summary>
         public string Text { [DebuggerStepThrough] get; }
     }
 }
