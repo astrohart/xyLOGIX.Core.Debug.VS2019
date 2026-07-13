56af8580410bd840261cf648a03e17c6af728740
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:25:17 -0600

    Update IRollingFileAppenderConfiguration.cs
    - Reflowed XML documentation for properties, values, and remarks.
    - Reformatted auto-properties for improved readability and consistency.
    - Consolidated line wrapping throughout the rolling file appender configuration documentation.

--- xyLOGIX.Core.Debug/IRollingFileAppenderConfiguration.cs
+++ xyLOGIX.Core.Debug/IRollingFileAppenderConfiguration.cs
@@ -5,8 +5,8 @@ using System.Diagnostics;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Defines the publicly-exposed events, methods and properties of an object that
-    /// encapsulates the configuration for a
+    /// Defines the publicly-exposed events, methods and properties of an
+    /// object that encapsulates the configuration for a
     /// <see cref="T:log4net.Appender.RollingFileAppender" />.
     /// </summary>
     public interface IRollingFileAppenderConfiguration
@@ -19,39 +19,32 @@ namespace xyLOGIX.Core.Debug
         /// <remarks>
         /// <para>
         /// If the value is set to <see langword="false" /> then the file will be
-        /// overwritten, if  it is set to <see langword="true" /> then the file will be
+        /// overwritten, if it is set to <see langword="true" /> then the file will be
         /// appended to.
         /// </para>
         /// The default value is <see langword="true" />.
         /// </remarks>
-        bool AppendToFile
-        {
-            [DebuggerStepThrough] get;
-            [DebuggerStepThrough] set;
-        }
+        bool AppendToFile { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
 
-        /// <summary>
-        /// Gets or sets the path to the file that logging will be written to.
-        /// </summary>
-        /// <value>
-        /// The path to the file that logging will be written to.
-        /// </value>
+        /// <summary>Gets or sets the path to the file that logging will be written to.</summary>
+        /// <value>The path to the file that logging will be written to.</value>
         /// <remarks>
         ///     <para>
-        ///     If the path is relative it is taken as relative from
-        ///     the application base directory.
+        ///     If the path is relative it is taken as relative from the application
+        ///     base directory.
         ///     </para>
         /// </remarks>
         string File { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
 
         /// <summary>
-        /// Gets or sets the <see cref="T:log4net.Layout.ILayout" /> for this appender.
+        /// Gets or sets the <see cref="T:log4net.Layout.ILayout" /> for this
+        /// appender.
         /// </summary>
         /// <value>The layout of the appender.</value>
         /// <remarks>
         ///     <para>
-        ///     See <see cref="P:log4net.Appender.AppenderSkeleton.RequiresLayout" /> for
-        ///     more information.
+        ///     See <see cref="P:log4net.Appender.AppenderSkeleton.RequiresLayout" />
+        ///     for more information.
         ///     </para>
         /// </remarks>
         /// <seealso cref="RequiresLayout" />
@@ -68,21 +61,13 @@ namespace xyLOGIX.Core.Debug
         /// <remarks>
         ///     <para>
         ///     This property allows you to specify the maximum size with the
-        ///     suffixes "KB", "MB" or "GB" so that the size is interpreted being
-        ///     expressed respectively in kilobytes, megabytes or gigabytes.
+        ///     suffixes "KB", "MB" or "GB" so that the size is interpreted being expressed
+        ///     respectively in kilobytes, megabytes or gigabytes.
         ///     </para>
-        ///     <para>
-        ///     For example, the value "10KB" will be interpreted as 10240 bytes.
-        ///     </para>
-        ///     <para>
-        ///     The default maximum file size is 10MB.
-        ///     </para>
+        ///     <para>For example, the value "10KB" will be interpreted as 10240 bytes.</para>
+        ///     <para>The default maximum file size is 10MB.</para>
         /// </remarks>
-        string MaximumFileSize
-        {
-            [DebuggerStepThrough] get;
-            [DebuggerStepThrough] set;
-        }
+        string MaximumFileSize { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
 
         /// <summary>
         /// Gets or sets the maximum number of backup files that are kept before
@@ -99,9 +84,9 @@ namespace xyLOGIX.Core.Debug
         ///     <see cref="P:log4net.Appender.RollingFileAppender.MaxFileSize" />.
         ///     </para>
         ///     <para>
-        ///     If a negative number is supplied then no deletions will be made.
-        ///     Note that this could result in very slow performance as a large number of
-        ///     files are rolled over unless
+        ///     If a negative number is supplied then no deletions will be made. Note
+        ///     that this could result in very slow performance as a large number of files
+        ///     are rolled over unless
         ///     <see cref="P:log4net.Appender.RollingFileAppender.CountDirection" /> is
         ///     used.
         ///     </para>
@@ -110,11 +95,7 @@ namespace xyLOGIX.Core.Debug
         ///     <b>not</b> the total.
         ///     </para>
         /// </remarks>
-        int MaxSizeRollBackups
-        {
-            [DebuggerStepThrough] get;
-            [DebuggerStepThrough] set;
-        }
+        int MaxSizeRollBackups { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
 
         /// <summary>Gets or sets the rolling style.</summary>
         /// <value>The rolling style.</value>
@@ -135,21 +116,23 @@ namespace xyLOGIX.Core.Debug
         /// </remarks>
         RollingFileAppender.RollingMode RollingStyle
         {
-            [DebuggerStepThrough] get;
-            [DebuggerStepThrough] set;
+            [DebuggerStepThrough]
+            get;
+            [DebuggerStepThrough]
+            set;
         }
 
         /// <summary>
-        /// Gets or sets a value indicating whether to always log to
-        /// the same file.
+        /// Gets or sets a value indicating whether to always log to the same
+        /// file.
         /// </summary>
         /// <value>
-        /// <see langword="true" /> if always should be logged to the same file, otherwise
-        /// <see langword="false" />.
+        /// <see langword="true" /> if always should be logged to the same file,
+        /// otherwise <see langword="false" />.
         /// </value>
         /// <remarks>
         ///     <para>
-        ///     By default, file.log is always the current file.  Optionally
+        ///     By default, file.log is always the current file. Optionally
         ///     file.log.yyyy-mm-dd for current formatted datePattern can by the currently
         ///     logging file (or file.log.curSizeRollBackup or even
         ///     file.log.yyyy-mm-dd.curSizeRollBackup).
@@ -159,10 +142,6 @@ namespace xyLOGIX.Core.Debug
         ///     much faster as the appender it won't have to rename all the backups!
         ///     </para>
         /// </remarks>
-        bool StaticLogFileName
-        {
-            [DebuggerStepThrough] get;
-            [DebuggerStepThrough] set;
-        }
+        bool StaticLogFileName { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
     }
 }
