73b92f99e571aa6265181385019859014fad6130
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:50:54 -0600

    Update LoggingInfrastructureTypeValidator.cs
    - Reformatted XML documentation comments for improved readability.
    - Consolidated singleton property and constructor formatting.
    - Rewrapped block and inline comments for consistent formatting.
    - Preserved the existing validation behavior and public API of `LoggingInfrastructureTypeValidator`.

--- xyLOGIX.Core.Debug/LoggingInfrastructureTypeValidator.cs
+++ xyLOGIX.Core.Debug/LoggingInfrastructureTypeValidator.cs
@@ -5,11 +5,10 @@ using System.Diagnostics;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Validates whether certain value(s) are within the defined value set of the
-    /// <see cref="T:xyLOGIX.Core.Debug.LoggingInfrastructureType" /> enumeration.
+    /// Validates whether certain value(s) are within the defined value set of
+    /// the <see cref="T:xyLOGIX.Core.Debug.LoggingInfrastructureType" /> enumeration.
     /// </summary>
-    internal class
-        LoggingInfrastructureTypeValidator : ILoggingInfrastructureTypeValidator
+    internal class LoggingInfrastructureTypeValidator : ILoggingInfrastructureTypeValidator
     {
         /// <summary>
         /// Empty, <see langword="static" /> constructor to prohibit direct
@@ -19,12 +18,12 @@ namespace xyLOGIX.Core.Debug
         static LoggingInfrastructureTypeValidator() { }
 
         /// <summary>
-        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of
-        /// this
-        /// class.
+        /// Empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
-        protected LoggingInfrastructureTypeValidator() { }
+        protected LoggingInfrastructureTypeValidator()
+        { }
 
         /// <summary>
         /// Gets a reference to the one and only instance of the object that
@@ -34,7 +33,8 @@ namespace xyLOGIX.Core.Debug
         /// </summary>
         internal static ILoggingInfrastructureTypeValidator Instance
         {
-            [DebuggerStepThrough] get;
+            [DebuggerStepThrough]
+            get;
         } = new LoggingInfrastructureTypeValidator();
 
         /// <summary>
@@ -45,8 +45,7 @@ namespace xyLOGIX.Core.Debug
         /// <param name="type">
         /// (Required.) One of the
         /// <see cref="T:xyLOGIX.Core.Debug.LoggingInfrastructureType" /> value(s) that is
-        /// to
-        /// be examined.
+        /// to be examined.
         /// </param>
         /// <returns>
         /// <see langword="true" /> if the logging infrastructure
@@ -64,30 +63,26 @@ namespace xyLOGIX.Core.Debug
                     $"LoggingInfrastructureTypeValidator.IsValid: type = '{type}'"
                 );
 
-                /*
-                 * For cybersecurity reasons, and to defeat reverse-engineering,
-                 * check the value of the 'type' parameter to ensure that it
-                 * is not set to a value outside the set of valid values defined
-                 * by the xyLOGIX.Core.Debug.LoggingInfrastructureType
-                 * enumeration.
-                 *
-                 * In principle, since all C# enums devolve to integer values, a
-                 * hacker could insert a different value into the CPU register that the
-                 * 'type' parameter is read from and thereby make this application
-                 * do something it's not intended to do.
+                /* For cybersecurity reasons, and to defeat reverse-engineering, check the value of
+                 the 'type' parameter to ensure that it is not set to a value outside the set of
+                 valid values defined by the xyLOGIX.Core.Debug.LoggingInfrastructureType
+                 enumeration. In principle, since all C# enums devolve to integer values, a hacker
+                 could insert a different value into the CPU register that the 'type' parameter is
+                 read from and thereby make this application do something it's not intended to do.
                  */
 
                 System.Diagnostics.Debug.WriteLine(
                     $"LoggingInfrastructureTypeValidator.IsValid: Checking whether the value of the 'type' parameter, i.e., '{type}', is within the defined value set of its enumerated data type..."
                 );
 
-                // Check whether the value of the 'type' parameter is within the defined value set of its 
-                // enumeration data type.  If this is not the case, then write an error message to the log
-                // file, and then terminate the execution of this method while returning the default return
-                // value.
+                // Check whether the value of the 'type' parameter is within the defined value set
+                // of its  enumeration data type.  If this is not the case, then write an error
+                // message to the log file, and then terminate the execution of this method while
+                // returning the default return value.
                 if (!Enum.IsDefined(typeof(LoggingInfrastructureType), type))
                 {
-                    // The value of the 'type' parameter is NOT within the defined value set for its enumerated data type.  This is not desirable.
+                    // The value of the 'type' parameter is NOT within the defined value set for its
+                    // enumerated data type.  This is not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         $"*** ERROR *** The value of the 'type' parameter, i.e., '{type}', is NOT within the defined value set of its enumerated data type.  Stopping..."
                     );
@@ -108,12 +103,14 @@ namespace xyLOGIX.Core.Debug
                     "LoggingInfrastructureTypeValidator.IsValid: Checking whether the 'Unknown' value has NOT been specified for the 'type' parameter..."
                 );
 
-                // Check whether the 'Unknown' value has been specified for the 'type' parameter.  If this is the case, then
-                // write an error message to the log file, and then terminate the execution of this method, returning the default
-                // return value in order to indicate that this method failed.
+                // Check whether the 'Unknown' value has been specified for the 'type' parameter.
+                // If this is the case, then write an error message to the log file, and then
+                // terminate the execution of this method, returning the default return value in
+                // order to indicate that this method failed.
                 if (LoggingInfrastructureType.Unknown.Equals(type))
                 {
-                    // The 'Unknown' value has been specified for the 'type' parameter.  This is not desirable.
+                    // The 'Unknown' value has been specified for the 'type' parameter.  This is not
+                    // desirable.
                     System.Diagnostics.Debug.WriteLine(
                         "*** ERROR *** The 'Unknown' value has been specified for the 'type' parameter.  Stopping..."
                     );
@@ -130,9 +127,7 @@ namespace xyLOGIX.Core.Debug
                     "LoggingInfrastructureTypeValidator.IsValid: *** SUCCESS *** The 'Unknown' value has NOT been specified for the 'type' parameter.  Proceeding..."
                 );
 
-                /*
-                 * If we made it here, then assume that the input data is valid.
-                 */
+                /* If we made it here, then assume that the input data is valid. */
 
                 result = true;
             }
