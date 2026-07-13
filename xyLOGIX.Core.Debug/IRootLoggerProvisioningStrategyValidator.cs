c21a2521ca46bbd109516f24303421f65d5049b1
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:26:00 -0600

    Update IRootLoggerProvisioningStrategyValidator.cs
    - Reflowed the interface summary XML documentation for improved readability.
    - Wrapped the `RootLoggerProvisioningStrategy` validator description across multiple lines.
    - Consolidated the `strategy` parameter documentation without changing its meaning.

--- xyLOGIX.Core.Debug/IRootLoggerProvisioningStrategyValidator.cs
+++ xyLOGIX.Core.Debug/IRootLoggerProvisioningStrategyValidator.cs
@@ -1,9 +1,9 @@
 ﻿namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Defines the publicly-exposed events, methods and properties of a validator of
-    /// <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" /> enumeration
-    /// values.
+    /// Defines the publicly-exposed events, methods and properties of a
+    /// validator of <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" />
+    /// enumeration values.
     /// </summary>
     /// <remarks>
     /// Specifically, objects that implement this interface ascertain whether
@@ -21,8 +21,7 @@
         /// <param name="strategy">
         /// (Required.) One of the
         /// <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" /> value(s)
-        /// that
-        /// is to be examined.
+        /// that is to be examined.
         /// </param>
         /// <returns>
         /// <see langword="true" /> if the root-logger provisioning
