49c7edc1ddd74dafea23adca8cfc42ce86ebcc12
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:56:08 -0600

    Update RootLoggerProvisioningStrategyValidator.cs
    - Reformatted XML documentation comments for improved readability.
    - Consolidated singleton property, constructor, and class declaration formatting.
    - Rewrapped block and inline comments for consistent formatting.
    - Preserved the existing validation behavior and public API of `RootLoggerProvisioningStrategyValidator`.

--- xyLOGIX.Core.Debug/RootLoggerProvisioningStrategyValidator.cs
+++ xyLOGIX.Core.Debug/RootLoggerProvisioningStrategyValidator.cs
@@ -5,13 +5,13 @@ using System.Diagnostics;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Validates whether certain value(s) are within the defined value set of the
-    /// <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" /> enumeration.
+    /// Validates whether certain value(s) are within the defined value set of
+    /// the <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" />
+    /// enumeration.
     /// </summary>
     [Log(AttributeExclude = true)]
-    internal class
-        RootLoggerProvisioningStrategyValidator :
-        IRootLoggerProvisioningStrategyValidator
+    internal class RootLoggerProvisioningStrategyValidator
+        : IRootLoggerProvisioningStrategyValidator
     {
         /// <summary>
         /// Empty, <see langword="static" /> constructor to prohibit direct
@@ -20,12 +20,12 @@ namespace xyLOGIX.Core.Debug
         static RootLoggerProvisioningStrategyValidator() { }
 
         /// <summary>
-        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of
-        /// this
-        /// class.
+        /// Empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
-        private RootLoggerProvisioningStrategyValidator() { }
+        private RootLoggerProvisioningStrategyValidator()
+        { }
 
         /// <summary>
         /// Gets a reference to the one and only instance of the object that implements the
@@ -35,7 +35,8 @@ namespace xyLOGIX.Core.Debug
         /// </summary>
         internal static IRootLoggerProvisioningStrategyValidator Instance
         {
-            [DebuggerStepThrough] get;
+            [DebuggerStepThrough]
+            get;
         } = new RootLoggerProvisioningStrategyValidator();
 
         /// <summary>
@@ -47,8 +48,7 @@ namespace xyLOGIX.Core.Debug
         /// <param name="strategy">
         /// (Required.) One of the
         /// <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" /> value(s)
-        /// that
-        /// is to be examined.
+        /// that is to be examined.
         /// </param>
         /// <returns>
         /// <see langword="true" /> if the root-logger provisioning
@@ -66,32 +66,26 @@ namespace xyLOGIX.Core.Debug
                     $"RootLoggerProvisioningStrategyValidator.IsValid: strategy = '{strategy}'"
                 );
 
-                /*
-                 * For cybersecurity reasons, and to defeat reverse-engineering,
-                 * check the value of the 'strategy' parameter to ensure that it
-                 * is not set to a value outside the set of valid values defined
-                 * by the xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy
-                 * enumeration.
-                 *
-                 * In principle, since all C# enums devolve to integer values, a
-                 * hacker could insert a different value into the CPU register that the
-                 * 'strategy' parameter is read from and thereby make this application
-                 * do something it's not intended to do.
-                 */
+                /* For cybersecurity reasons, and to defeat reverse-engineering, check the value of
+                 the 'strategy' parameter to ensure that it is not set to a value outside the set of
+                 valid values defined by the xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy
+                 enumeration. In principle, since all C# enums devolve to integer values, a hacker
+                 could insert a different value into the CPU register that the 'strategy' parameter
+                 is read from and thereby make this application do something it's not intended to
+                 do. */
 
                 System.Diagnostics.Debug.WriteLine(
                     $"RootLoggerProvisioningStrategyValidator.IsValid: Checking whether the value of the 'strategy' parameter, i.e., '{strategy}', is within the defined value set of its enumerated data type..."
                 );
 
-                // Check whether the value of the 'strategy' parameter is within the defined value set of its 
-                // enumeration data type.  If this is not the case, then write an error message to the log
-                // file, and then terminate the execution of this method while returning the default return
-                // value.
-                if (!Enum.IsDefined(
-                        typeof(RootLoggerProvisioningStrategy), strategy
-                    ))
+                // Check whether the value of the 'strategy' parameter is within the defined value
+                // set of its  enumeration data type.  If this is not the case, then write an error
+                // message to the log file, and then terminate the execution of this method while
+                // returning the default return value.
+                if (!Enum.IsDefined(typeof(RootLoggerProvisioningStrategy), strategy))
                 {
-                    // The value of the 'strategy' parameter is NOT within the defined value set for its enumerated data type.  This is not desirable.
+                    // The value of the 'strategy' parameter is NOT within the defined value set for
+                    // its enumerated data type.  This is not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         $"*** ERROR *** The value of the 'strategy' parameter, i.e., '{strategy}', is NOT within the defined value set of its enumerated data type.  Stopping..."
                     );
@@ -112,12 +106,14 @@ namespace xyLOGIX.Core.Debug
                     "RootLoggerProvisioningStrategyValidator.IsValid: Checking whether the 'Unknown' value has NOT been specified for the 'strategy' parameter..."
                 );
 
-                // Check whether the 'Unknown' value has been specified for the 'strategy' parameter.  If this is the case, then
-                // write an error message to the log file, and then terminate the execution of this method, returning the default
-                // return value in order to indicate that this method failed.
+                // Check whether the 'Unknown' value has been specified for the 'strategy'
+                // parameter.  If this is the case, then write an error message to the log file, and
+                // then terminate the execution of this method, returning the default return value
+                // in order to indicate that this method failed.
                 if (RootLoggerProvisioningStrategy.Unknown.Equals(strategy))
                 {
-                    // The 'Unknown' value has been specified for the 'strategy' parameter.  This is not desirable.
+                    // The 'Unknown' value has been specified for the 'strategy' parameter.  This is
+                    // not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         "*** ERROR *** The 'Unknown' value has been specified for the 'strategy' parameter.  Stopping..."
                     );
@@ -134,9 +130,7 @@ namespace xyLOGIX.Core.Debug
                     "RootLoggerProvisioningStrategyValidator.IsValid: *** SUCCESS *** The 'Unknown' value has NOT been specified for the 'strategy' parameter.  Proceeding..."
                 );
 
-                /*
-                 * If we made it here, then assume that the input data is valid.
-                 */
+                /* If we made it here, then assume that the input data is valid. */
 
                 result = true;
             }
