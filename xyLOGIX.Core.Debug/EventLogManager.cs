76b235a9917620b3af2aea04add37d362bfdc40e
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:32:35 -0600

    Update EventLogManager.cs
    - Reflowed XML documentation, inline comments, and block comments for improved readability.
    - Reformatted property and method declarations for improved consistency.
    - Consolidated line wrapping throughout event log management logic without changing behavior.

--- xyLOGIX.Core.Debug/EventLogManager.cs
+++ xyLOGIX.Core.Debug/EventLogManager.cs
@@ -5,21 +5,19 @@ using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
-    /// <summary> Class to manage access to the event log. </summary>
+    /// <summary>Class to manage access to the event log.</summary>
     [Log(AttributeExclude = true), ExplicitlySynchronized]
     internal class EventLogManager : IEventLogManager
     {
         /// <summary>
-        /// Empty, <see langword="static" /> constructor to prohibit direct allocation of
-        /// this
-        /// class.
+        /// Empty, <see langword="static" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         static EventLogManager() { }
 
         /// <summary>
-        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of
-        /// this
-        /// class.
+        /// Empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
         private EventLogManager()
@@ -33,19 +31,16 @@ namespace xyLOGIX.Core.Debug
         /// Gets a reference to an instance of an object that implements the
         /// <see cref="T:xyLOGIX.Core.Debug.IEventLogTypeValidator" /> interface.
         /// </summary>
-        private static IEventLogTypeValidator EventLogTypeValidator
-        {
-            [DebuggerStepThrough] get;
-        } = GetEventLogTypeValidator.SoleInstance();
+        private static IEventLogTypeValidator EventLogTypeValidator { [DebuggerStepThrough] get; } =
+            GetEventLogTypeValidator.SoleInstance();
 
         /// <summary>
         /// Gets a reference to the one and only instance of the object that
         /// implements the <see cref="T:xyLOGIX.Core.Debug.IEventLogManager" /> interface
-        /// that
-        /// manages our access to the Windows System Event Logs.
+        /// that manages our access to the Windows System Event Logs.
         /// </summary>
-        internal static IEventLogManager
-            Instance { [DebuggerStepThrough] get; } = new EventLogManager();
+        internal static IEventLogManager Instance { [DebuggerStepThrough] get; } =
+            new EventLogManager();
 
         /// <summary>
         /// Gets a value indicating whether this object has been properly
@@ -62,8 +57,7 @@ namespace xyLOGIX.Core.Debug
                 {
                     if (string.IsNullOrWhiteSpace(Source)) return result;
 
-                    result = !EventLogType.None.Equals(Type) &&
-                             !EventLogType.Unknown.Equals(Type);
+                    result = !EventLogType.None.Equals(Type) && !EventLogType.Unknown.Equals(Type);
                 }
                 catch (Exception ex)
                 {
@@ -85,11 +79,7 @@ namespace xyLOGIX.Core.Debug
         /// This property must be set before logging events, otherwise an error
         /// will occur.
         /// </remarks>
-        public string Source
-        {
-            [DebuggerStepThrough] get;
-            [DebuggerStepThrough] private set;
-        }
+        public string Source { [DebuggerStepThrough] get; [DebuggerStepThrough] private set; }
 
         /// <summary>
         /// Gets or sets the type of log to which events are to be sent
@@ -99,29 +89,23 @@ namespace xyLOGIX.Core.Debug
         /// This property must be set before logging events, otherwise an error
         /// will occur.
         /// </remarks>
-        public EventLogType Type
-        {
-            [DebuggerStepThrough] get;
-            [DebuggerStepThrough] private set;
-        }
+        public EventLogType Type { [DebuggerStepThrough] get; [DebuggerStepThrough] private set; }
 
         /// <summary>
         /// Sends an Error event to the system event log pointed to by the
         /// <see cref="P:xyLOGIX.Core.Debug.EventLogManager.Source" /> and
         /// <see cref="P:xyLOGIX.Core.Debug.EventLogManager.Type" /> properties. The
-        /// content of
-        /// the logging message is specified by the <paramref name="content" /> parameter.
+        /// content of the logging message is specified by the <paramref name="content" />
+        /// parameter.
         /// </summary>
-        /// <param name="content"> String specifying the content of the event log message. </param>
+        /// <param name="content">String specifying the content of the event log message.</param>
         public void Error([NotLogged] string content)
         {
             try
             {
-                /*
-                 * NOTE: Since it is expected that the implementation of this
-                 * method is fairly straightforward, we will not be using the
-                 * logging infrastructure to log the progress of this method.
-                 */
+                /* NOTE: Since it is expected that the implementation of this method is fairly
+                 straightforward, we will not be using the logging infrastructure to log the
+                 progress of this method. */
 
                 if (string.IsNullOrWhiteSpace(content)) return;
                 if (!IsInitialized) return;
@@ -139,8 +123,8 @@ namespace xyLOGIX.Core.Debug
         /// Sends an Info event to the system event log pointed to by the
         /// <see cref="P:xyLOGIX.Core.Debug.EventLogManager.Source" /> and
         /// <see cref="P:xyLOGIX.Core.Debug.EventLogManager.Type" /> properties. The
-        /// content of
-        /// the logging message is specified by the <paramref name="content" /> parameter.
+        /// content of the logging message is specified by the <paramref name="content" />
+        /// parameter.
         /// </summary>
         /// <param name="content">
         /// (Required.) String specifying the content of the event
@@ -150,18 +134,14 @@ namespace xyLOGIX.Core.Debug
         {
             try
             {
-                /*
-                 * NOTE: Since it is expected that the implementation of this
-                 * method is fairly straightforward, we will not be using the
-                 * logging infrastructure to log the progress of this method.
-                 */
+                /* NOTE: Since it is expected that the implementation of this method is fairly
+                 straightforward, we will not be using the logging infrastructure to log the
+                 progress of this method. */
 
                 if (string.IsNullOrWhiteSpace(content)) return;
                 if (!IsInitialized) return;
 
-                EventLog.WriteEntry(
-                    Source, content, EventLogEntryType.Information
-                );
+                EventLog.WriteEntry(Source, content, EventLogEntryType.Information);
             }
             catch (Exception ex)
             {
@@ -170,7 +150,7 @@ namespace xyLOGIX.Core.Debug
             }
         }
 
-        /// <summary> Initializes event logging for your application. </summary>
+        /// <summary>Initializes event logging for your application.</summary>
         /// <param name="eventSourceName">
         /// (Required.) String containing the name of the
         /// application that will be sending events.
@@ -178,17 +158,13 @@ namespace xyLOGIX.Core.Debug
         /// <param name="logType">
         /// (Required.) One of the
         /// <see cref="T:xyLOGIX.Core.Debug.Constants.EventLogType" /> value(s) that
-        /// specifies the
-        /// type of log to send events to.
+        /// specifies the type of log to send events to.
         /// </param>
         /// <returns>
         /// <see langword="true" /> if the operation(s) completed successfully;
         /// <see langword="false" /> otherwise.
         /// </returns>
-        public bool Initialize(
-            [NotLogged] string eventSourceName,
-            EventLogType logType
-        )
+        public bool Initialize([NotLogged] string eventSourceName, EventLogType logType)
         {
             var result = false;
 
@@ -203,12 +179,13 @@ namespace xyLOGIX.Core.Debug
                     "EventLogManager.Initialize *** INFO: Checking whether the value of the parameter, 'eventSourceName', is blank..."
                 );
 
-                // Check whether the value of the parameter, 'eventSourceName', is blank.
-                // If this is so, then emit an error message to the log file, and
-                // then terminate the execution of this method.
+                // Check whether the value of the parameter, 'eventSourceName', is blank. If this is
+                // so, then emit an error message to the log file, and then terminate the execution
+                // of this method.
                 if (string.IsNullOrWhiteSpace(eventSourceName))
                 {
-                    // The parameter, 'eventSourceName' was either passed a null value, or it is blank.  This is not desirable.
+                    // The parameter, 'eventSourceName' was either passed a null value, or it is
+                    // blank.  This is not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         "EventLogManager.Initialize: *** ERROR *** The parameter, 'eventSourceName' was either passed a null value, or it is blank. Stopping..."
                     );
@@ -234,12 +211,13 @@ namespace xyLOGIX.Core.Debug
                     $"EventLogManager.Initialize: Checking whether the type of event log, '{logType}', is within the defined value set..."
                 );
 
-                // Check to see whether the type of event log is within the defined value set.
-                // If this is not the case, then write an error message to the log file,
-                // and then terminate the execution of this method.
+                // Check to see whether the type of event log is within the defined value set. If
+                // this is not the case, then write an error message to the log file, and then
+                // terminate the execution of this method.
                 if (!EventLogTypeValidator.IsValid(logType))
                 {
-                    // The type of event log is NOT within the defined value set.  This is not desirable.
+                    // The type of event log is NOT within the defined value set.  This is not
+                    // desirable.
                     System.Diagnostics.Debug.WriteLine(
                         $"*** ERROR *** The type of event log, '{logType}', is NOT within the defined value set.  Stopping..."
                     );
@@ -256,32 +234,29 @@ namespace xyLOGIX.Core.Debug
                     $"EventLogManager.Initialize: *** SUCCESS *** The type of event log, '{logType}', is within the defined value set.  Proceeding..."
                 );
 
-
                 System.Diagnostics.Debug.WriteLine(
                     $"*** EventLogManager.Initialize: Checking whether the Event Log already has a source called, '{eventSourceName}'..."
                 );
 
                 // Check to see whether the Event Log already has a source by the name of
-                // 'eventSourceName'.  If this is not the case, then write an error message
-                // to the Debug log and then terminate the execution of this method.
+                // 'eventSourceName'.  If this is not the case, then write an error message to the
+                // Debug log and then terminate the execution of this method.
                 if (!EventLog.SourceExists(eventSourceName))
                 {
-                    // No Event Source having the name 'eventSourceName' is currently configured.  Attempt to create such a source.
+                    // No Event Source having the name 'eventSourceName' is currently configured.
+                    // Attempt to create such a source.
                     System.Diagnostics.Debug.WriteLine(
                         $"*** WARNING: No Event Source having the name '{eventSourceName}' is currently configured.  Attempting to create such a source..."
                     );
 
-                    EventLog.CreateEventSource(
-                        eventSourceName, logType.ToString()
-                    );
+                    EventLog.CreateEventSource(eventSourceName, logType.ToString());
 
                     System.Diagnostics.Debug.WriteLine(
                         $"EventLogManager.Initialize: *** SUCCESS *** The Event Source, '{eventSourceName}', has been successfully created.  Proceeding..."
                     );
 
                     DebugUtils.WriteLine(
-                        DebugLevel.Debug,
-                        $"EventLogManager.Initialize: Result = {true}"
+                        DebugLevel.Debug, $"EventLogManager.Initialize: Result = {true}"
                     );
 
                     // stop.
@@ -292,22 +267,20 @@ namespace xyLOGIX.Core.Debug
                     $"EventLogManager.Initialize: *** SUCCESS *** The Event Log already has a source called, '{eventSourceName}'.  Proceeding..."
                 );
 
-                // Finally, save the event quote and type settings in the
-                // Source and Type properties.
+                // Finally, save the event quote and type settings in the Source and Type
+                // properties.
                 Source = eventSourceName;
                 Type = logType;
 
-                /*
-                 * If we made it this far with no Exception(s) getting caught, then
-                 * assume that the operation(s) succeeded.
-                 */
+                /* If we made it this far with no Exception(s) getting caught, then assume that the
+                 operation(s) succeeded. */
 
                 result = true;
             }
             catch (Exception ex)
             {
-                // If an exception was caught, de-initialize the member
-                // prevent this class from working in future calls.
+                // If an exception was caught, de-initialize the member prevent this class from
+                // working in future calls.
 
                 Source = string.Empty;
                 Type = EventLogType.Unknown;
@@ -317,9 +290,7 @@ namespace xyLOGIX.Core.Debug
                 result = false;
             }
 
-            System.Diagnostics.Debug.WriteLine(
-                $"EventLogManager.Initialize: Result = {result}"
-            );
+            System.Diagnostics.Debug.WriteLine($"EventLogManager.Initialize: Result = {result}");
 
             return result;
         }
@@ -328,19 +299,17 @@ namespace xyLOGIX.Core.Debug
         /// Sends a Warning event to the system event log pointed to by the
         /// <see cref="P:xyLOGIX.Core.Debug.EventLogManager.Source" /> and
         /// <see cref="P:xyLOGIX.Core.Debug.EventLogManager.Type" /> properties. The
-        /// content of
-        /// the logging message is specified by the <paramref name="content" /> parameter.
+        /// content of the logging message is specified by the <paramref name="content" />
+        /// parameter.
         /// </summary>
-        /// <param name="content"> String specifying the content of the event log message. </param>
+        /// <param name="content">String specifying the content of the event log message.</param>
         public void Warn([NotLogged] string content)
         {
             try
             {
-                /*
-                 * NOTE: Since it is expected that the implementation of this
-                 * method is fairly straightforward, we will not be using the
-                 * logging infrastructure to log the progress of this method.
-                 */
+                /* NOTE: Since it is expected that the implementation of this method is fairly
+                 straightforward, we will not be using the logging infrastructure to log the
+                 progress of this method. */
 
                 if (string.IsNullOrWhiteSpace(content)) return;
                 if (!IsInitialized) return;
