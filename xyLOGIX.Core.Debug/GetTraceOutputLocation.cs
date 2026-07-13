3a405d906905d62bb012966d9f2faa66eac671e6
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:47:02 -0600

    Update GetTraceOutputLocation.cs
    - Reformatted XML documentation comments for improved readability.
    - Rewrapped summary, constructor, and return-value documentation for consistent formatting.
    - Preserved the existing behavior and public API of `GetTraceOutputLocation`.

--- xyLOGIX.Core.Debug/GetTraceOutputLocation.cs
+++ xyLOGIX.Core.Debug/GetTraceOutputLocation.cs
@@ -7,8 +7,7 @@ namespace xyLOGIX.Core.Debug
     /// <summary>
     /// Provides access to the one and only instance of the object that
     /// implements the <see cref="T:xyLOGIX.Core.Debug.IOutputLocation" /> interface
-    /// that
-    /// directs debugging output to the <b>Output</b> window in Visual Studio or
+    /// that directs debugging output to the <b>Output</b> window in Visual Studio or
     /// whichever other debugger can listen to the output of the
     /// <see cref="T:System.Diagnostics.Trace" /> class' methods.
     /// </summary>
@@ -16,12 +15,13 @@ namespace xyLOGIX.Core.Debug
     internal static class GetTraceOutputLocation
     {
         /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.GetTraceOutputLocation" /> class.
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:xyLOGIX.Core.Debug.GetTraceOutputLocation" />
+        /// class.
         /// </summary>
         /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
         /// </remarks>
         [Log(AttributeExclude = true)]
         static GetTraceOutputLocation() { }
@@ -32,11 +32,10 @@ namespace xyLOGIX.Core.Debug
         /// reference to it.
         /// </summary>
         /// <returns>
-        /// Reference to the one, and only, instance of the object that
-        /// implements the <see cref="T:xyLOGIX.Core.Debug.IOutputLocation" /> interface
-        /// that
-        /// directs debugging output to the <b>Output</b> window in Visual Studio or
-        /// whichever other debugger can listen to the output of the
+        /// Reference to the one, and only, instance of the object that implements
+        /// the <see cref="T:xyLOGIX.Core.Debug.IOutputLocation" /> interface that directs
+        /// debugging output to the <b>Output</b> window in Visual Studio or whichever
+        /// other debugger can listen to the output of the
         /// <see cref="T:System.Diagnostics.Trace" /> class' methods.
         /// </returns>
         [DebuggerStepThrough]
