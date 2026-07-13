862e309fa9ea755653973cc1987da333733c139e
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:59:10 -0600

    Update TextEmittedEventArgs.cs
    - Reformatted XML documentation for improved readability and consistency.
    - Condensed the `TextEmittedEventArgs` constructor declaration onto a single line.
    - Simplified the `Level` property documentation formatting.

--- xyLOGIX.Core.Debug/TextEmittedEventArgs.cs
+++ xyLOGIX.Core.Debug/TextEmittedEventArgs.cs
@@ -5,17 +5,18 @@ using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
-    /// <summary> Provides information for <c>TextEmitted</c> event handlers. </summary>
+    /// <summary>Provides information for <c>TextEmitted</c> event handlers.</summary>
     [ExplicitlySynchronized]
     public class TextEmittedEventArgs : EventArgs
     {
         /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.TextEmittedEventArgs" /> class.
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:xyLOGIX.Core.Debug.TextEmittedEventArgs" />
+        /// class.
         /// </summary>
         /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
         /// <para />
         /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
         /// attribute in order to simplify the logging output.
@@ -37,18 +38,15 @@ namespace xyLOGIX.Core.Debug
         /// enumeration values that indicates of what level of severity is the message.
         /// </param>
         [Log(AttributeExclude = true)]
-        public TextEmittedEventArgs(
-            [NotLogged] string text,
-            [NotLogged] DebugLevel level
-        )
+        public TextEmittedEventArgs([NotLogged] string text, [NotLogged] DebugLevel level)
         {
             Text = text;
             Level = level;
         }
 
         /// <summary>
-        /// Gets of the <see cref="T:xyLOGIX.Core.Debug.DebugLevel" />
-        /// enumeration values that indicates of what level of severity is the message.
+        /// Gets of the <see cref="T:xyLOGIX.Core.Debug.DebugLevel" /> enumeration
+        /// values that indicates of what level of severity is the message.
         /// </summary>
         public DebugLevel Level { [DebuggerStepThrough] get; }
 
