de7f7d8bd2f9889920dc6dfc63af3c869b2c8c80
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:47:58 -0600

    Update Has.cs
    - Reformatted XML documentation comments for improved readability.
    - Consolidated property, method declaration, and collection initializer formatting.
    - Rewrapped inline comments for consistent formatting.
    - Simplified conditional expressions and `Debug.WriteLine` invocations without changing behavior.

--- xyLOGIX.Core.Debug/Has.cs
+++ xyLOGIX.Core.Debug/Has.cs
@@ -9,39 +9,34 @@ using System.Reflection;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Exposes <see langword="static" /> methods to determine whether a console window
-    /// is
-    /// present.
+    /// Exposes <see langword="static" /> methods to determine whether a
+    /// console window is present.
     /// </summary>
     [ExplicitlySynchronized]
     public static class Has
     {
         /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Core.Debug.Has" /> class.
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the <see cref="T:xyLOGIX.Core.Debug.Has" /> class.
         /// </summary>
         /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
         /// </remarks>
         [Log(AttributeExclude = true)]
         static Has() { }
 
         /// <summary>
-        /// Gets or sets a value that is initialized with a <see cref="T:System.Boolean" />
-        /// that indicates whether the calling application is a Windows GUI app or a
-        /// console app.
+        /// Gets or sets a value that is initialized with a
+        /// <see cref="T:System.Boolean" /> that indicates whether the calling application
+        /// is a Windows GUI app or a console app.
         /// </summary>
         /// <remarks>
-        /// We use this property to avoid re-determining the nature of the current app if
-        /// the <see cref="M:xyLOGIX.Core.Debug.Has.WindowsGui" /> method is called more
-        /// than once.
+        /// We use this property to avoid re-determining the nature of the current
+        /// app if the <see cref="M:xyLOGIX.Core.Debug.Has.WindowsGui" /> method is called
+        /// more than once.
         /// </remarks>
-        private static bool? IsWindowsGUI
-        {
-            [DebuggerStepThrough] get;
-            [DebuggerStepThrough] set;
-        }
+        private static bool? IsWindowsGUI { [DebuggerStepThrough] get; [DebuggerStepThrough] set; }
 
         /// <summary>
         /// Determines whether the application is currently running in a console
@@ -53,7 +48,7 @@ namespace xyLOGIX.Core.Debug
         /// </returns>
         /// <remarks>
         /// This method first tries to get the value of the
-        /// <see cref="P:System.Console.WindowHeight" /> property.  If this throws an
+        /// <see cref="P:System.Console.WindowHeight" /> property. If this throws an
         /// exception, we return <see langword="false" /> since we aren't running in a
         /// console window.
         /// <para />
@@ -63,9 +58,9 @@ namespace xyLOGIX.Core.Debug
         /// This algorithm assures us we get an accurate response, i.e.,
         /// <see langword="false" />, from this method if the caller is not running in a
         /// console and is NOT GUI-based; i.e., it is like a console app, but it is set to
-        /// <c>Windows Application</c> type in the <b>Project Properties</b>
-        /// window in Visual Studio, but it never creates a window (at least, not using WPF
-        /// or WinForms; this method cannot detect native Win32 P/Invoke calls to create a
+        /// <c>Windows Application</c> type in the <b>Project Properties</b> window in
+        /// Visual Studio, but it never creates a window (at least, not using WPF or
+        /// WinForms; this method cannot detect native Win32 P/Invoke calls to create a
         /// main window).
         /// </remarks>
         [DebuggerStepThrough]
@@ -87,9 +82,7 @@ namespace xyLOGIX.Core.Debug
             return result;
         }
 
-        /// <summary>
-        /// Determines whether logging has been configured.
-        /// </summary>
+        /// <summary>Determines whether logging has been configured.</summary>
         /// <returns>
         /// <see langword="true" /> if a backend has been assigned to the value of
         /// the
@@ -113,12 +106,14 @@ namespace xyLOGIX.Core.Debug
                     "Has.LoggingBeenSetUp: Checking whether the property, 'LoggingServices.DefaultBackend', has a null reference for a value..."
                 );
 
-                // Check to see if the required property, 'LoggingServices.DefaultBackend', has a null reference for a value.
-                // If that is the case, then we will write an error message to the log file, and then
-                // terminate the execution of this method, while returning the default return value.
+                // Check to see if the required property, 'LoggingServices.DefaultBackend', has a
+                // null reference for a value. If that is the case, then we will write an error
+                // message to the log file, and then terminate the execution of this method, while
+                // returning the default return value.
                 if (LoggingServices.DefaultBackend == null)
                 {
-                    // The property, 'LoggingServices.DefaultBackend', has a null reference for a value.  This is not desirable.
+                    // The property, 'LoggingServices.DefaultBackend', has a null reference for a
+                    // value.  This is not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         "Has.LoggingBeenSetUp: *** ERROR *** The property, 'LoggingServices.DefaultBackend', has a null reference for a value.  Stopping..."
                     );
@@ -143,8 +138,7 @@ namespace xyLOGIX.Core.Debug
                     "Has.LoggingBeenSetUp: *** FYI *** Determining whether the property, 'LoggingServices.DefaultBackend', is NOT set to 'NullLoggingBackend'..."
                 );
 
-                result =
-                    !(LoggingServices.DefaultBackend is NullLoggingBackend);
+                result = !(LoggingServices.DefaultBackend is NullLoggingBackend);
             }
             catch (Exception ex)
             {
@@ -154,9 +148,7 @@ namespace xyLOGIX.Core.Debug
                 result = false;
             }
 
-            System.Diagnostics.Debug.WriteLine(
-                $"Has.LoggingBeenSetUp: Result = {result}"
-            );
+            System.Diagnostics.Debug.WriteLine($"Has.LoggingBeenSetUp: Result = {result}");
 
             return result;
         }
@@ -169,7 +161,7 @@ namespace xyLOGIX.Core.Debug
         /// </summary>
         /// <param name="useEntryAssembly">
         /// (Optional.) Default is <see langword="false" />
-        /// .  If <see langword="true" />, uses the entry-point assembly of the application
+        /// . If <see langword="true" />, uses the entry-point assembly of the application
         /// to make its determination. Setting this parameter to <see langword="false" />
         /// means the calling assembly is used instead.
         /// </param>
@@ -203,20 +195,17 @@ namespace xyLOGIX.Core.Debug
 
                 var wpfAssemblies = new[]
                 {
-                    "PresentationFramework", "PresentationCore",
-                    "WindowsBase"
+                    "PresentationFramework", "PresentationCore", "WindowsBase"
                 };
                 var winFormsAssemblies = new[] { "System.Windows.Forms" };
 
-                var referencedAssemblies =
-                    assemblyToCheck.GetReferencedAssemblies();
+                var referencedAssemblies = assemblyToCheck.GetReferencedAssemblies();
                 if (referencedAssemblies == null || !referencedAssemblies.Any())
                     return IsWindowsGUI.Value;
 
                 foreach (var assemblyName in referencedAssemblies)
                 {
-                    if (assemblyName == null ||
-                        string.IsNullOrWhiteSpace(assemblyName.Name))
+                    if (assemblyName == null || string.IsNullOrWhiteSpace(assemblyName.Name))
                         return IsWindowsGUI.Value;
 
                     if (wpfAssemblies.Contains(assemblyName.Name))
