fd44f3576a373d7d30bc5af0118d7238e82d97b1
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:37:55 -0600

    Update FromLogManagerRootLoggerProvisioner.cs
    - Reformatted XML documentation comments for improved readability.
    - Consolidated singleton property, constructor, and method declaration formatting.
    - Simplified class declaration formatting.
    - Rewrapped documentation without changing `FromLogManagerRootLoggerProvisioner` behavior.

--- xyLOGIX.Core.Debug/FromLogManagerRootLoggerProvisioner.cs
+++ xyLOGIX.Core.Debug/FromLogManagerRootLoggerProvisioner.cs
@@ -7,51 +7,48 @@ using Logger = log4net.Repository.Hierarchy.Logger;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// This concrete class simply executes the fallback provisioning strategy and
-    /// attempts to return the value of such, ignoring any provided reference to a
+    /// This concrete class simply executes the fallback provisioning strategy
+    /// and attempts to return the value of such, ignoring any provided reference to a
     /// <c>Logging Repository</c>.
     /// </summary>
     [Log(AttributeExclude = true)]
-    internal class
-        FromLogManagerRootLoggerProvisioner : RootLoggerProvisionerBase
+    internal class FromLogManagerRootLoggerProvisioner : RootLoggerProvisionerBase
     {
         /// <summary>
-        /// Empty, <see langword="static" /> constructor to prohibit direct allocation of
-        /// this class.
+        /// Empty, <see langword="static" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         static FromLogManagerRootLoggerProvisioner() { }
 
         /// <summary>
-        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of
-        /// this class.
+        /// Empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
-        private FromLogManagerRootLoggerProvisioner() { }
+        private FromLogManagerRootLoggerProvisioner()
+        { }
 
         /// <summary>
-        /// Gets a reference to the one and only instance of the object that implements the
-        /// <see cref="T:xyLOGIX.Core.Debug.IRootLoggerProvisioner" /> interface for the
+        /// Gets a reference to the one and only instance of the object that
+        /// implements the <see cref="T:xyLOGIX.Core.Debug.IRootLoggerProvisioner" />
+        /// interface for the
         /// <see cref="F:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy.FromLogManager" />
         /// root-logger provisioning strategy.
         /// </summary>
-        internal static IRootLoggerProvisioner Instance
-        {
-            [DebuggerStepThrough] get;
-        } = new FromLogManagerRootLoggerProvisioner();
+        internal static IRootLoggerProvisioner Instance { [DebuggerStepThrough] get; } =
+            new FromLogManagerRootLoggerProvisioner();
 
         /// <summary>
-        /// Gets the <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" />
-        /// enumeration value that indicates the strategy used to provision the
-        /// <c>Root Logger</c>.
+        /// Gets the
+        /// <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" /> enumeration
+        /// value that indicates the strategy used to provision the <c>Root Logger</c>.
         /// </summary>
-        public override RootLoggerProvisioningStrategy Strategy
-        {
-            [DebuggerStepThrough] get;
-        } = RootLoggerProvisioningStrategy.FromLogManager;
+        public override RootLoggerProvisioningStrategy Strategy { [DebuggerStepThrough] get; } =
+            RootLoggerProvisioningStrategy.FromLogManager;
 
         /// <summary>
-        /// Provisions the <c>Root Logger</c> for the application depending on the value of
-        /// the <paramref name="loggerRepository" /> parameter.
+        /// Provisions the <c>Root Logger</c> for the application depending on the
+        /// value of the <paramref name="loggerRepository" /> parameter.
         /// </summary>
         /// <remarks>
         /// If the provided <paramref name="loggerRepository" /> can be directly
@@ -63,25 +60,22 @@ namespace xyLOGIX.Core.Debug
         /// If a <see langword="null" /> reference is passed for the value of the
         /// <paramref name="loggerRepository" /> parameter, then this method attempts to
         /// find the default appender configuration and attempts to then return a reference
-        /// to that configuration's
-        /// <see cref="T:log4net.Repository.Hierarchy.Logger" />.
+        /// to that configuration's <see cref="T:log4net.Repository.Hierarchy.Logger" />.
         /// <para />
         /// If the first two attempts fail, then this method returns a
         /// <see langword="null" /> reference.
         /// <para />
         /// If this particular <c>Root Logger Provisioner</c> is configured to use the
         /// <see cref="F:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy.FromLogManager" />
-        /// strategy, then this method attempts to find the default appender
-        /// configuration and attempts to then return a reference to that configuration's
+        /// strategy, then this method attempts to find the default appender configuration
+        /// and attempts to then return a reference to that configuration's
         /// <see cref="T:log4net.Repository.Hierarchy.Logger" />.
         /// <para />
         /// Failing that, a <see langword="null" /> reference is returned.
         /// </remarks>
         /// <param name="loggerRepository"></param>
         /// <returns></returns>
-        public override Logger Provision(
-            ILoggerRepository loggerRepository = null
-        )
+        public override Logger Provision(ILoggerRepository loggerRepository = null)
         {
             Logger result = default;
 
