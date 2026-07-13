e3f5c3b9c1c2b97be1ab3e773395e368db8449d0
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:38:09 -0600

    Update FromProvidedLoggingRepositoryRootLoggerProvisioner.cs
    - Reformatted XML documentation comments for improved readability.
    - Consolidated singleton property, constructor, and method declaration formatting.
    - Rewrapped block and inline comments for consistent style.
    - Simplified class declaration formatting without changing behavior.

--- xyLOGIX.Core.Debug/FromProvidedLoggingRepositoryRootLoggerProvisioner.cs
+++ xyLOGIX.Core.Debug/FromProvidedLoggingRepositoryRootLoggerProvisioner.cs
@@ -8,28 +8,27 @@ using Logger = log4net.Repository.Hierarchy.Logger;
 namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Attempts to provision the root logger from an existing <c>Logger Repository</c>
-    /// component that has been determined, in advance, to be convertible to a
-    /// reference to an instance of
+    /// Attempts to provision the root logger from an existing
+    /// <c>Logger Repository</c> component that has been determined, in advance, to be
+    /// convertible to a reference to an instance of
     /// <see cref="T:log4net.Repository.Hierarchy.Hierarchy" />.
     /// </summary>
     [Log(AttributeExclude = true)]
-    internal class
-        FromProvidedLoggingRepositoryRootLoggerProvisioner :
-        RootLoggerProvisionerBase
+    internal class FromProvidedLoggingRepositoryRootLoggerProvisioner : RootLoggerProvisionerBase
     {
         /// <summary>
-        /// Empty, <see langword="static" /> constructor to prohibit direct allocation of
-        /// this class.
+        /// Empty, <see langword="static" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         static FromProvidedLoggingRepositoryRootLoggerProvisioner() { }
 
         /// <summary>
-        /// Empty, <see langword="private" /> constructor to prohibit direct allocation of
-        /// this class.
+        /// Empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
-        private FromProvidedLoggingRepositoryRootLoggerProvisioner() { }
+        private FromProvidedLoggingRepositoryRootLoggerProvisioner()
+        { }
 
         /// <summary>
         /// Gets a reference to the one and only instance of the object that implements the
@@ -38,24 +37,20 @@ namespace xyLOGIX.Core.Debug
         ///     cref="F:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy.FromProvidedLoggingRepository" />
         /// root-logger provisioning strategy.
         /// </summary>
-        internal static IRootLoggerProvisioner Instance
-        {
-            [DebuggerStepThrough] get;
-        } = new FromProvidedLoggingRepositoryRootLoggerProvisioner();
+        internal static IRootLoggerProvisioner Instance { [DebuggerStepThrough] get; } =
+            new FromProvidedLoggingRepositoryRootLoggerProvisioner();
 
         /// <summary>
-        /// Gets the <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" />
-        /// enumeration value that indicates the strategy used to provision the
-        /// <c>Root Logger</c>.
+        /// Gets the
+        /// <see cref="T:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy" /> enumeration
+        /// value that indicates the strategy used to provision the <c>Root Logger</c>.
         /// </summary>
-        public override RootLoggerProvisioningStrategy Strategy
-        {
-            [DebuggerStepThrough] get;
-        } = RootLoggerProvisioningStrategy.FromProvidedLoggingRepository;
+        public override RootLoggerProvisioningStrategy Strategy { [DebuggerStepThrough] get; } =
+            RootLoggerProvisioningStrategy.FromProvidedLoggingRepository;
 
         /// <summary>
-        /// Provisions the <c>Root Logger</c> for the application depending on the value of
-        /// the <paramref name="loggerRepository" /> parameter.
+        /// Provisions the <c>Root Logger</c> for the application depending on the
+        /// value of the <paramref name="loggerRepository" /> parameter.
         /// </summary>
         /// <remarks>
         /// If the provided <paramref name="loggerRepository" /> can be directly
@@ -67,45 +62,41 @@ namespace xyLOGIX.Core.Debug
         /// If a <see langword="null" /> reference is passed for the value of the
         /// <paramref name="loggerRepository" /> parameter, then this method attempts to
         /// find the default appender configuration and attempts to then return a reference
-        /// to that configuration's
-        /// <see cref="T:log4net.Repository.Hierarchy.Logger" />.
+        /// to that configuration's <see cref="T:log4net.Repository.Hierarchy.Logger" />.
         /// <para />
         /// If the first two attempts fail, then this method returns a
         /// <see langword="null" /> reference.
         /// <para />
         /// If this particular <c>Root Logger Provisioner</c> is configured to use the
         /// <see cref="F:xyLOGIX.Core.Debug.RootLoggerProvisioningStrategy.FromLogManager" />
-        /// strategy, then this method attempts to find the default appender
-        /// configuration and attempts to then return a reference to that configuration's
+        /// strategy, then this method attempts to find the default appender configuration
+        /// and attempts to then return a reference to that configuration's
         /// <see cref="T:log4net.Repository.Hierarchy.Logger" />.
         /// <para />
         /// Failing that, a <see langword="null" /> reference is returned.
         /// </remarks>
         /// <param name="loggerRepository"></param>
         /// <returns></returns>
-        public override Logger Provision(
-            ILoggerRepository loggerRepository = null
-        )
+        public override Logger Provision(ILoggerRepository loggerRepository = null)
         {
             Logger result = default;
 
             try
             {
-                /*
-                 * If we are here, then it has been determined that the specified value
-                 * of the loggerRepository parameter is castable to Hierarchy.
-                 */
+                /* If we are here, then it has been determined that the specified value of the
+                 loggerRepository parameter is castable to Hierarchy. */
 
                 System.Diagnostics.Debug.WriteLine(
                     "FromProvidedLoggingRepositoryRootLoggerProvisioner.Provision: Checking whether the 'loggerRepository' method parameter has a null reference for a value..."
                 );
 
-                // Check to see if the required parameter, loggerRepository, is null. If it is, send an
-                // error to the log file and quit, returning the default return value of this
+                // Check to see if the required parameter, loggerRepository, is null. If it is, send
+                // an error to the log file and quit, returning the default return value of this
                 // method.
                 if (loggerRepository == null)
                 {
-                    // The parameter, 'loggerRepository', is required and is not supposed to have a NULL value.
+                    // The parameter, 'loggerRepository', is required and is not supposed to have a
+                    // NULL value.
                     System.Diagnostics.Debug.WriteLine(
                         "FromProvidedLoggingRepositoryRootLoggerProvisioner.Provision: *** ERROR *** A null reference was passed for the 'loggerRepository' method parameter.  Attempting to get a reference to the default Hierarchy repository..."
                     );
@@ -122,12 +113,13 @@ namespace xyLOGIX.Core.Debug
                     "*** FromProvidedLoggingRepositoryRootLoggerProvisioner.Provision: Checking whether the provided Logger Repository is convertible to 'Hierarchy'..."
                 );
 
-                // Check to see whether the provided Logger Repository is convertible to 'Hierarchy'.
-                // If this is not the case, then write an error message to the log file,
-                // and then terminate the execution of this method.
+                // Check to see whether the provided Logger Repository is convertible to
+                // 'Hierarchy'. If this is not the case, then write an error message to the log
+                // file, and then terminate the execution of this method.
                 if (!(loggerRepository is Hierarchy hierarchyRepository))
                 {
-                    // The provided Logger Repository is NOT convertible to 'Hierarchy'.  This is not desirable.
+                    // The provided Logger Repository is NOT convertible to 'Hierarchy'.  This is
+                    // not desirable.
                     System.Diagnostics.Debug.WriteLine(
                         "*** ERROR *** The provided Logger Repository is NOT convertible to 'Hierarchy'.  Executing the Fallback Provisioning Strategy..."
                     );
@@ -144,12 +136,13 @@ namespace xyLOGIX.Core.Debug
                     "FromProvidedLoggingRepositoryRootLoggerProvisioner.Provision: Checking whether the variable, 'hierarchyRepository', has a null reference for a value..."
                 );
 
-                // Check to see if the variable, hierarchyRepository, is null.  If it is, send an error
-                // to the log file, and then terminate the execution of this method,
-                // returning the default return value.
+                // Check to see if the variable, hierarchyRepository, is null.  If it is, send an
+                // error to the log file, and then terminate the execution of this method, returning
+                // the default return value.
                 if (hierarchyRepository == null)
                 {
-                    // the variable hierarchyRepository is required to have a valid object reference.
+                    // the variable hierarchyRepository is required to have a valid object
+                    // reference.
                     System.Diagnostics.Debug.WriteLine(
                         "FromProvidedLoggingRepositoryRootLoggerProvisioner.Provision: *** ERROR ***  The variable, 'hierarchyRepository', has a null reference.  Executing the Fallback Provisioning Strategy..."
                     );
@@ -158,7 +151,8 @@ namespace xyLOGIX.Core.Debug
                     return ExecuteFallbackProvisioning();
                 }
 
-                // We can use the variable, hierarchyRepository, because it's not set to a null reference.
+                // We can use the variable, hierarchyRepository, because it's not set to a null
+                // reference.
                 System.Diagnostics.Debug.WriteLine(
                     "FromProvidedLoggingRepositoryRootLoggerProvisioner.Provision: *** SUCCESS *** The variable, 'hierarchyRepository', has a valid object reference for its value.  Proceeding..."
                 );
