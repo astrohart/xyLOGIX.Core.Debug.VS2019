0e9022ce858233595589cf1e2ea4666166f36e00
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 18:01:40 -0600

    Update XmlLoggingConfiguratorType.cs
    - Reformatted XML documentation for improved readability and consistent line wrapping.
    - Reflowed enumeration member documentation to follow the repository's preferred documentation style.
    - Simplified the `Unknown` enumeration member summary to the preferred single-line format.

--- xyLOGIX.Core.Debug/XmlLoggingConfiguratorType.cs
+++ xyLOGIX.Core.Debug/XmlLoggingConfiguratorType.cs
@@ -1,25 +1,24 @@
 ﻿namespace xyLOGIX.Core.Debug
 {
     /// <summary>
-    /// Value(s) that specify the manner in which the logging subsystem is configured.
+    /// Value(s) that specify the manner in which the logging subsystem is
+    /// configured.
     /// </summary>
     public enum XmlLoggingConfiguratorType
     {
         /// <summary>
-        /// An XML-formatted <c>app.config</c> or <c>web.config</c> file is available for
-        /// configuration of the logging subsystem.
+        /// An XML-formatted <c>app.config</c> or <c>web.config</c> file is
+        /// available for configuration of the logging subsystem.
         /// </summary>
         FileBased,
 
         /// <summary>
-        /// No XML-formatted <c>app.config</c> or <c>web.config</c> file is available for
-        /// configuration of the logging subsystem.
+        /// No XML-formatted <c>app.config</c> or <c>web.config</c> file is
+        /// available for configuration of the logging subsystem.
         /// </summary>
         NoFile,
 
-        /// <summary>
-        /// Unknown XML logging configurator type.
-        /// </summary>
+        /// <summary>Unknown XML logging configurator type.</summary>
         Unknown = -1
     }
 }
