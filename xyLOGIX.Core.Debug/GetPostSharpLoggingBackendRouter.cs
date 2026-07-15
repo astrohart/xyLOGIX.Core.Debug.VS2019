705ebdf2164a7cfc7cc23257e6d3507ded2070f6
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-15 02:37:18 -0600

    Implement GetPostSharpLoggingBackendRouter factory
    - Convert `GetPostSharpLoggingBackendRouter` into a static singleton factory for the shared PostSharp logging backend router.
    - Add the `SoleInstance()` accessor to retrieve the shared `IPostSharpLoggingBackendRouter` implementation.
    - Document the factory and accessor with repository-standard XML documentation and `PostSharp` logging annotations.
    - Handle router acquisition failures by writing diagnostic information to the Debug output and returning the default result.

--- xyLOGIX.Core.Debug/GetPostSharpLoggingBackendRouter.cs
+++ xyLOGIX.Core.Debug/GetPostSharpLoggingBackendRouter.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
 
 namespace xyLOGIX.Core.Debug
 {
-    internal class GetPostSharpLoggingBackendRouter
+    /// <summary>Provides access to the sole PostSharp logging-backend router instance.</summary>
+    [Log(AttributeExclude = true)]
+    internal static class GetPostSharpLoggingBackendRouter
     {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.GetPostSharpLoggingBackendRouter" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetPostSharpLoggingBackendRouter() { }
+
+        /// <summary>
+        /// Gets a reference to the sole instance of an object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.IPostSharpLoggingBackendRouter" /> interface.
+        /// </summary>
+        /// <returns>
+        /// Reference to the sole router instance; otherwise,
+        /// <see langword="null" />.
+        /// </returns>
+        /// <remarks>
+        /// If the router cannot be obtained, the exception information is written
+        /// to the Debug output and this method returns <see langword="null" />.
+        /// </remarks>
+        [return: NotLogged]
+        internal static IPostSharpLoggingBackendRouter SoleInstance()
+        {
+            IPostSharpLoggingBackendRouter result;
+
+            try
+            {
+                result = PostSharpLoggingBackendRouter.Instance;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
     }
 }
