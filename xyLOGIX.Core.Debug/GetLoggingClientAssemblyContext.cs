4ec49110626416ebe2f7d185c9e1c1861aef8061
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-14 20:11:28 -0600

    Implement GetLoggingClientAssemblyContext factory
    - Convert `GetLoggingClientAssemblyContext` into a static singleton factory for the logging client assembly context.
    - Add the `SoleInstance()` accessor to return the shared `ILoggingClientAssemblyContext` implementation.
    - Document the factory and accessor with repository-standard XML documentation and `PostSharp` logging annotations.
    - Handle context acquisition failures by logging exceptions to the Debug output and returning the default result.

--- xyLOGIX.Core.Debug/GetLoggingClientAssemblyContext.cs
+++ xyLOGIX.Core.Debug/GetLoggingClientAssemblyContext.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
-    internal class GetLoggingClientAssemblyContext
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientAssemblyContext" /> interface.
+    /// </summary>
+    internal static class GetLoggingClientAssemblyContext
     {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.GetLoggingClientAssemblyContext" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetLoggingClientAssemblyContext() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientAssemblyContext" /> interface,
+        /// and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientAssemblyContext" /> interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        internal static ILoggingClientAssemblyContext SoleInstance()
+        {
+            ILoggingClientAssemblyContext result;
+
+            try
+            {
+                result = LoggingClientAssemblyContext.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the Debug output
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
     }
 }
