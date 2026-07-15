0152f3653e9dce899c0576fbb377f9e589a144db
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-15 17:44:03 -0600

    Create FetchLegacyLoggerSessionLoggerFetcher.cs
    - Added the `FetchLegacyLoggerSessionLoggerFetcher` implementation deriving from `SessionLoggerFetcherBase`.
    - Implemented the singleton `Instance` property for the `FetchLegacyLogger` fetch approach.
    - Overrode the `Approach` property to identify the legacy logger retrieval strategy.
    - Added the `FetchLogger` override stub to establish the concrete fetcher contract.
    - Documented the class, singleton lifecycle, and legacy logger-fetching behavior with XML documentation.

---
+++ xyLOGIX.Core.Debug/FetchLegacyLoggerSessionLoggerFetcher.cs
@@ -0,0 +1,112 @@
+﻿using log4net;
+using PostSharp.Patterns.Diagnostics;
+using PostSharp.Patterns.Threading;
+using System;
+using System.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
+{
+    /// <summary>
+    /// Gets a reference to an instance of an object that implements the
+    /// <see cref="T:log4net.ILog" /> interface for a client session by using the
+    /// specified <paramref name="sourceType" /> and optional
+    /// <paramref name="repositoryName" />, if specified, per the
+    /// <see cref="F:xyLOGIX.Core.Debug.SessionLoggerFetchApproach.FetchLegacyLogger" />
+    /// approach.
+    /// </summary>
+    [Log(AttributeExclude = true), ExplicitlySynchronized]
+    internal class FetchLegacyLoggerSessionLoggerFetcher : SessionLoggerFetcherBase
+    {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.FetchLegacyLoggerSessionLoggerFetcher" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static FetchLegacyLoggerSessionLoggerFetcher() { }
+
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:xyLOGIX.Core.Debug.FetchLegacyLoggerSessionLoggerFetcher" /> and
+        /// returns a reference to it.
+        /// </summary>
+        /// <remarks>
+        /// This is an empty, <see langword="private" /> constructor to prohibit
+        /// direct allocation of this class, as it is a <c>Singleton</c> object accessible
+        /// via the
+        /// <see cref="P:xyLOGIX.Core.Debug.FetchLegacyLoggerSessionLoggerFetcher.Instance" />
+        /// property.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        private FetchLegacyLoggerSessionLoggerFetcher()
+        { }
+
+        /// <summary>
+        /// Gets the
+        /// <see cref="T:xyLOGIX.Core.Debug.SessionLoggerFetchApproach" /> enumeration
+        /// value that identifies the particular approach that is to be utilized to fetch a
+        /// logger for a client session, or to use the legacy logger if no other approach
+        /// is available.
+        /// </summary>
+        public override SessionLoggerFetchApproach Approach { [DebuggerStepThrough] get; } =
+            SessionLoggerFetchApproach.FetchLegacyLogger;
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that
+        /// implements the <see cref="T:xyLOGIX.Core.Debug.ISessionLoggerFetcher" />
+        /// interface for the
+        /// <see cref="F:xyLOGIX.Core.Debug.SessionLoggerFetchApproach.FetchLegacyLogger" />
+        /// approach.
+        /// </summary>
+        internal static ISessionLoggerFetcher Instance { [DebuggerStepThrough] get; } =
+            new FetchLegacyLoggerSessionLoggerFetcher();
+
+        /// <summary>
+        /// Attempts to fetch a logger for a client session by using specified
+        /// <paramref name="sourceType" /> and optional <paramref name="repositoryName" />,
+        /// if specified, per the
+        /// <see cref="T:xyLOGIX.Core.Debug.SessionLoggerFetchApproach" /> enumeration
+        /// value that is specified by the
+        /// <see cref="P:xyLOGIX.Core.Debug.ISessionLoggerFetcher.Approach" /> property.
+        /// </summary>
+        /// <param name="sourceType">
+        /// (Required.) Reference to an instance of
+        /// <see cref="T:System.Type" /> that is utilized to identify the logger that is to
+        /// be fetched for a client session.
+        /// <para />
+        /// Perhaps this value might come from an internal cache.
+        /// </param>
+        /// <param name="repositoryName">
+        /// (Optional.) A <see cref="T:System.String" /> that
+        /// is set to the name of the repository that is associated with the logger that is
+        /// to be fetched for a client session.
+        /// <para />
+        /// If this value is not specified, then the default repository name will be used.
+        /// <para />
+        /// The default value of this parameter is the <see cref="F:System.String.Empty" />
+        /// value.
+        /// <para />
+        /// The repository name is only to be provided when a more refined search is to be
+        /// employed.
+        /// <para />
+        /// In such a case where we are searching on the repository name, then the value of
+        /// the <paramref name="repositoryName" /> parameter must be specified, and it must
+        /// not be <see cref="F:System.String.Empty" />.
+        /// </param>
+        /// <returns>
+        /// If successful, a reference to an instance of an object that implements
+        /// the <see cref="T:log4net.ILog" /> interface; otherwise, a
+        /// <see langword="null" /> reference is returned.
+        /// </returns>
+        public override ILog FetchLogger(Type sourceType, string repositoryName = "")
+            => null;
+    }
+}
