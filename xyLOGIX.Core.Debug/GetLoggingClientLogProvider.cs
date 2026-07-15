86c3cadb66b096c21b9aeb1e29bdf5a3f02dce28
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-15 10:55:55 -0600

    Implement GetLoggingClientLogProvider factory
    - Convert `GetLoggingClientLogProvider` into a static singleton factory for the shared logging client log provider.
    - Add the `SoleInstance()` accessor to retrieve the shared `ILoggingClientLogProvider` implementation.
    - Document the factory and accessor with repository-standard XML documentation and `PostSharp` logging annotations.
    - Handle provider acquisition failures by writing diagnostic information to the Debug output and returning the default result.

--- xyLOGIX.Core.Debug/GetLoggingClientLogProvider.cs
+++ xyLOGIX.Core.Debug/GetLoggingClientLogProvider.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
 
 namespace xyLOGIX.Core.Debug
 {
-    internal class GetLoggingClientLogProvider
+    /// <summary>Provides access to the sole logging-client log-provider object.</summary>
+    [Log(AttributeExclude = true)]
+    internal static class GetLoggingClientLogProvider
     {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.GetLoggingClientLogProvider" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically before any
+        /// <see langword="static" /> member is referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetLoggingClientLogProvider() { }
+
+        /// <summary>
+        /// Gets a reference to the sole instance of an object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLogProvider" /> interface.
+        /// </summary>
+        /// <returns>
+        /// Reference to the sole instance of an object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLogProvider" /> interface;
+        /// otherwise, <see langword="null" />.
+        /// </returns>
+        /// <remarks>
+        /// If the provider cannot be obtained, then the exception information is
+        /// written to the Debug output and this method returns <see langword="null" />.
+        /// </remarks>
+        [return: NotLogged]
+        internal static ILoggingClientLogProvider SoleInstance()
+        {
+            ILoggingClientLogProvider result;
+
+            try
+            {
+                result = LoggingClientLogProvider.Instance;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
     }
 }
