cf2defd748c1a468f171d1d56f0f1af5c602b0ff
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 17:27:37 -0600

    Update GetLoggingClientLoggerCacheAddOutcomeValidator.cs
    - Implement the singleton validator factory for cache add-outcome validation.
    - Convert the placeholder class into a static factory type.
    - Add `SoleInstance()` to return the shared `ILoggingClientLoggerCacheAddOutcomeValidator` implementation.
    - Add XML documentation, logging attributes, and `DebuggerStepThrough` annotations.
    - Add defensive exception handling when resolving the singleton validator instance.

--- xyLOGIX.Core.Debug/GetLoggingClientLoggerCacheAddOutcomeValidator.cs
+++ xyLOGIX.Core.Debug/GetLoggingClientLoggerCacheAddOutcomeValidator.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
-    internal class GetLoggingClientLoggerCacheAddOutcomeValidator
+    /// <summary>
+    /// Provides access to the one and only instance of the object that
+    /// implements the
+    /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheAddOutcomeValidator" />
+    /// interface.
+    /// </summary>
+    [Log(AttributeExclude = true)]
+    internal static class GetLoggingClientLoggerCacheAddOutcomeValidator
     {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.GetLoggingClientLoggerCacheAddOutcomeValidator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> member(s) are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetLoggingClientLoggerCacheAddOutcomeValidator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheAddOutcomeValidator" />
+        /// interface, and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements
+        /// the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheAddOutcomeValidator" />
+        /// interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        internal static ILoggingClientLoggerCacheAddOutcomeValidator SoleInstance()
+        {
+            ILoggingClientLoggerCacheAddOutcomeValidator result;
+
+            try
+            {
+                result = LoggingClientLoggerCacheAddOutcomeValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
     }
 }
