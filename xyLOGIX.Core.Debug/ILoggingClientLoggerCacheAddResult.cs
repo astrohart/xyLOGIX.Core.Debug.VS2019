d6c93318f17bee0cde2a3ef74a3061483f48c216
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 17:04:16 -0600

    Update ILoggingClientLoggerCacheAddResult.cs
    - Define the `ILoggingClientLoggerCacheAddResult` contract.
    - Add `HandlerType` and `Outcome` properties describing cache-add processing and results.
    - Document the distinction between the Handler Chain decision and the final cache-add outcome.
    - Add XML documentation and `DebuggerStepThrough` annotations to the interface properties.

--- xyLOGIX.Core.Debug/ILoggingClientLoggerCacheAddResult.cs
+++ xyLOGIX.Core.Debug/ILoggingClientLoggerCacheAddResult.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of an
+    /// object that describes the result of an attempt to add a logger to the
+    /// logging-client logger cache.
+    /// </summary>
     internal interface ILoggingClientLoggerCacheAddResult
     {
+        /// <summary>
+        /// Gets a
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddHandlerType" />
+        /// enumeration value that identifies the Handler Chain link that accepted
+        /// responsibility for the cache-add operation.
+        /// </summary>
+        LoggingClientLoggerCacheAddHandlerType HandlerType { [DebuggerStepThrough] get; }
+
+        /// <summary>
+        /// Gets a
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddOutcome" />
+        /// enumeration value that describes the final outcome of the cache-add operation.
+        /// </summary>
+        LoggingClientLoggerCacheAddOutcome Outcome { [DebuggerStepThrough] get; }
     }
 }
