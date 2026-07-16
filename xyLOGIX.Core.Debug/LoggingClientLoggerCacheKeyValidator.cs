e4ed36ec90afeaa79edcc147ad67a0524783cbd9
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 13:51:37 -0600

    Update LoggingClientLoggerCacheKeyValidator.cs
    - Implement the `ILoggingClientLoggerCacheKeyValidator` interface.
    - Convert the validator into a singleton with a private constructor and shared `Instance` property.
    - Add XML documentation for the validator lifecycle and singleton access pattern.
    - Add logging attributes and `DebuggerStepThrough` annotations to singleton members.

--- xyLOGIX.Core.Debug/LoggingClientLoggerCacheKeyValidator.cs
+++ xyLOGIX.Core.Debug/LoggingClientLoggerCacheKeyValidator.cs
@@ -1,4 +1,48 @@
-﻿namespace xyLOGIX.Core.Debug
+﻿using PostSharp.Patterns.Diagnostics;
+using System.Diagnostics;
+
+namespace xyLOGIX.Core.Debug
 {
-    internal class LoggingClientLoggerCacheKeyValidator { }
+    internal class LoggingClientLoggerCacheKeyValidator : ILoggingClientLoggerCacheKeyValidator
+    {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheKeyValidator" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static LoggingClientLoggerCacheKeyValidator() { }
+
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheKeyValidator" /> and
+        /// returns a reference to it.
+        /// </summary>
+        /// <remarks>
+        /// This is an empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class, as it is a <c>Singleton</c> object accessible via the
+        /// <see cref="P:xyLOGIX.Core.Debug.LoggingClientLoggerCacheKeyValidator.Instance" />
+        /// property.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        private LoggingClientLoggerCacheKeyValidator() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that
+        /// implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheKeyValidator" />
+        /// interface.
+        /// </summary>
+        internal static ILoggingClientLoggerCacheKeyValidator Instance
+        {
+            [DebuggerStepThrough] get;
+        } = new LoggingClientLoggerCacheKeyValidator();
+    }
 }
