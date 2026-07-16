33a99b9c0e6208694b8bde556673be22bd09b2c2
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 16:58:28 -0600

    Update LoggingClientLoggerCacheAddHandlerTypeValidator.cs
    - Implement the `ILoggingClientLoggerCacheAddHandlerTypeValidator` interface.
    - Convert the validator into a singleton with a private constructor and shared `Instance` property.
    - Add XML documentation for the validator lifecycle and singleton access pattern.
    - Add logging attributes and `DebuggerStepThrough` annotations to singleton members.

--- xyLOGIX.Core.Debug/LoggingClientLoggerCacheAddHandlerTypeValidator.cs
+++ xyLOGIX.Core.Debug/LoggingClientLoggerCacheAddHandlerTypeValidator.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
+using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
     internal class LoggingClientLoggerCacheAddHandlerTypeValidator
+        : ILoggingClientLoggerCacheAddHandlerTypeValidator
     {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that need to be
+        /// performed once only for the
+        /// <see
+        ///     cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddHandlerTypeValidator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static LoggingClientLoggerCacheAddHandlerTypeValidator() { }
+
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see
+        ///     cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddHandlerTypeValidator" />
+        /// and returns a reference to it.
+        /// </summary>
+        /// <remarks>
+        /// This is an empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class, as it is a <c>Singleton</c> object accessible via the
+        /// <see
+        ///     cref="P:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddHandlerTypeValidator.Instance" />
+        /// property.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        private LoggingClientLoggerCacheAddHandlerTypeValidator() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that implements the
+        /// <see
+        ///     cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheAddHandlerTypeValidator" />
+        /// interface.
+        /// </summary>
+        internal static ILoggingClientLoggerCacheAddHandlerTypeValidator Instance
+        {
+            [DebuggerStepThrough] get;
+        } = new LoggingClientLoggerCacheAddHandlerTypeValidator();
     }
 }
