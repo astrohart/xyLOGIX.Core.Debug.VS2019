373845c660307fb5148a47cb9696dced721d3fd0
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 12:08:49 -0600

    Update ILoggingClientLoggerCacheKeyBuilder.cs
    - Define the `ILoggingClientLoggerCacheKeyBuilder` contract.
    - Add `AndLoggerNamed(...)` to complete logger-cache key construction.
    - Specify builder behavior for invalid logger-name values.
    - Add XML documentation and `NotLogged` annotations for the builder method.

--- xyLOGIX.Core.Debug/ILoggingClientLoggerCacheKeyBuilder.cs
+++ xyLOGIX.Core.Debug/ILoggingClientLoggerCacheKeyBuilder.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a
+    /// component that completes the construction of a logging-client logger-cache key.
+    /// </summary>
     internal interface ILoggingClientLoggerCacheKeyBuilder
     {
+        /// <summary>
+        /// Creates a new logging-client logger-cache key for the repository
+        /// previously supplied to the builder and the specified
+        /// <paramref name="loggerName" />.
+        /// </summary>
+        /// <param name="loggerName">
+        /// (Required.) A <see cref="T:System.String" /> value
+        /// containing the name of the logger within the repository previously supplied to
+        /// the builder.
+        /// </param>
+        /// <remarks>
+        /// If a <see langword="null" />, blank, or
+        /// <see cref="F:System.String.Empty" /> value is passed for the
+        /// <paramref name="loggerName" /> parameter, then this method returns a
+        /// <see langword="null" /> reference.
+        /// </remarks>
+        /// <returns>
+        /// If successful, a reference to an instance of an object that implements
+        /// the <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheKey" /> interface;
+        /// otherwise, a <see langword="null" /> reference is returned.
+        /// </returns>
+        [return: NotLogged]
+        ILoggingClientLoggerCacheKey AndLoggerNamed([NotLogged] string loggerName);
     }
 }
