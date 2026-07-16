548658c211c7cafe46da22e888fc55086b0ccd5b
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 17:25:11 -0600

    Update ILoggingClientLoggerCacheAddOutcomeValidator.cs
    - Define the `ILoggingClientLoggerCacheAddOutcomeValidator` contract.
    - Add `IsValid(...)` to validate logging-client logger cache add-outcome values.
    - Document validator responsibilities, validation semantics, and failure conditions.
    - Specify that undefined and `Unknown` outcome values are treated as invalid.

--- xyLOGIX.Core.Debug/ILoggingClientLoggerCacheAddOutcomeValidator.cs
+++ xyLOGIX.Core.Debug/ILoggingClientLoggerCacheAddOutcomeValidator.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace xyLOGIX.Core.Debug
+﻿namespace xyLOGIX.Core.Debug
 {
+    /// <summary>
+    /// Defines the internally-exposed events, methods and properties of a
+    /// validator of
+    /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddOutcome" />
+    /// enumeration values.
+    /// </summary>
+    /// <remarks>
+    /// Specifically, objects that implement this interface ascertain whether
+    /// the values of variables fall within the value set that is defined by the
+    /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddOutcome" />
+    /// enumeration.
+    /// </remarks>
     internal interface ILoggingClientLoggerCacheAddOutcomeValidator
     {
+        /// <summary>
+        /// Determines whether the logging-client logger-cache <c>Add</c>
+        /// <paramref name="outcome" /> value passed is within the value set that is
+        /// defined by the
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddOutcome" />
+        /// enumeration.
+        /// </summary>
+        /// <param name="outcome">
+        /// (Required.) One of the
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddOutcome" /> values
+        /// that is to be examined.
+        /// </param>
+        /// <remarks>
+        /// If the value of the <paramref name="outcome" /> parameter is outside
+        /// the defined value set, or is equal to
+        /// <see cref="F:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddOutcome.Unknown" />,
+        /// then this method returns <see langword="false" />.
+        /// </remarks>
+        /// <returns>
+        /// <see langword="true" /> if the logging-client logger-cache <c>Add</c>
+        /// <paramref name="outcome" /> falls within the defined value set and is not equal
+        /// to
+        /// <see cref="F:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddOutcome.Unknown" />;
+        /// otherwise, <see langword="false" />.
+        /// </returns>
+        bool IsValid(LoggingClientLoggerCacheAddOutcome outcome);
     }
 }
