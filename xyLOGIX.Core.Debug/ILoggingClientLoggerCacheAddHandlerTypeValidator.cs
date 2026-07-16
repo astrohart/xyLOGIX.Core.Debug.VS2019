040bd60ebb58536a032bbebcabecd496959e5fd8
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 17:03:14 -0600

    Update ILoggingClientLoggerCacheAddHandlerTypeValidator.cs
    - Remove the redundant silent validation method from the validator contract.
    - Consolidate add-handler type validation behind a single `IsValid(...)` entry point.
    - Simplify the validator interface by eliminating duplicate API surface.

--- xyLOGIX.Core.Debug/ILoggingClientLoggerCacheAddHandlerTypeValidator.cs
+++ xyLOGIX.Core.Debug/ILoggingClientLoggerCacheAddHandlerTypeValidator.cs
@@ -31,24 +31,5 @@
         /// <see langword="false" /> otherwise.
         /// </returns>
         bool IsValid(LoggingClientLoggerCacheAddHandlerType type);
-
-        /// <summary>
-        /// Determines whether the logging-client logger cache <c>Add</c> handler
-        /// <paramref name="type" /> value passed is within the value set that is defined
-        /// by the
-        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddHandlerType" />
-        /// enumeration.
-        /// </summary>
-        /// <param name="type">
-        /// (Required.) One of the
-        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddHandlerType" />
-        /// values that is to be examined.
-        /// </param>
-        /// <returns>
-        /// <see langword="true" /> if the logging-client logger cache <c>Add</c>
-        /// handler <paramref name="type" /> falls within the defined value set;
-        /// <see langword="false" /> otherwise.
-        /// </returns>
-        bool IsValidSilent(LoggingClientLoggerCacheAddHandlerType type);
     }
 }
