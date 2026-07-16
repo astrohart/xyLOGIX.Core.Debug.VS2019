7c087ee7a53553f8402961d5337faa8e70e8c21a
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 11:59:37 -0600

    Update ILoggingClientLoggerCacheKey.cs
    - Define the `ILoggingClientLoggerCacheKey` contract.
    - Add `Repository` and `LoggerName` properties to identify cached loggers.
    - Document logger-cache key identity semantics and repository association.
    - Add XML documentation and `DebuggerStepThrough` annotations to the interface properties.

--- xyLOGIX.Core.Debug/ILoggingClientLoggerCacheKey.cs
+++ xyLOGIX.Core.Debug/ILoggingClientLoggerCacheKey.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using log4net.Repository;
+using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of an
+    /// object that uniquely identifies a logger within a particular log4net
+    /// repository.
+    /// </summary>
+    /// <remarks>
+    /// Implementers identify a logger by combining the object identity of its
+    /// repository with its ordinal logger name.
+    /// <para />
+    /// Two key object(s) that refer to the same repository instance and contain the
+    /// same logger name represent the same logical cache entry.
+    /// </remarks>
     internal interface ILoggingClientLoggerCacheKey
     {
+        /// <summary>
+        /// Gets a <see cref="T:System.String" /> containing the name of the
+        /// logger within the repository identified by the
+        /// <see cref="P:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheKey.Repository" />
+        /// property.
+        /// </summary>
+        string LoggerName { [DebuggerStepThrough] get; }
+
+        /// <summary>
+        /// Gets a reference to an instance of an object that implements the
+        /// <see cref="T:log4net.Repository.ILoggerRepository" /> interface that contains
+        /// the logger identified by the
+        /// <see cref="P:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheKey.LoggerName" />
+        /// property.
+        /// </summary>
+        ILoggerRepository Repository { [DebuggerStepThrough] get; }
     }
 }
