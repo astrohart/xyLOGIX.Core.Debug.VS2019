263dbc8d7995e50bbd2abaceb2b07466e26a4497
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 11:45:59 -0600

    Update GetLoggingClientLoggerCache.cs
    - Implement the `GetLoggingClientLoggerCache` singleton factory.
    - Convert the placeholder class into a static factory type.
    - Add `SoleInstance()` to return the shared `ILoggingClientLoggerCache` implementation.
    - Add XML documentation, logging attributes, and debugger-step-through annotations.
    - Add defensive exception handling when resolving the singleton instance.

--- xyLOGIX.Core.Debug/GetLoggingClientLoggerCache.cs
+++ xyLOGIX.Core.Debug/GetLoggingClientLoggerCache.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
-    internal class GetLoggingClientLoggerCache
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCache" /> interface.
+    /// </summary>
+    [Log(AttributeExclude = true)]
+    internal static class GetLoggingClientLoggerCache
     {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.GetLoggingClientLoggerCache" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetLoggingClientLoggerCache() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCache" /> interface, and
+        /// returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCache" /> interface.
+        /// </returns>
+        [DebuggerStepThrough, Log(AttributeExclude = true)]
+        [return: NotLogged]
+        internal static ILoggingClientLoggerCache SoleInstance()
+        {
+            ILoggingClientLoggerCache result;
+
+            try
+            {
+                result = LoggingClientLoggerCache.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the Debug output
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
     }
 }
