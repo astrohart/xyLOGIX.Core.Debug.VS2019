e52384423e98f82e7bea75d3a09265eb8b875923
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 16:53:29 -0600

    Update LoggingClientLoggerCacheAddResult.cs
    - Implement the `ILoggingClientLoggerCacheAddResult` interface.
    - Convert the add-result type into a singleton with a private constructor and shared `Instance` property.
    - Add XML documentation for the singleton lifecycle and access pattern.
    - Add logging attributes and `DebuggerStepThrough` annotations to singleton members.

--- xyLOGIX.Core.Debug/LoggingClientLoggerCacheAddResult.cs
+++ xyLOGIX.Core.Debug/LoggingClientLoggerCacheAddResult.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
+using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
-    internal class LoggingClientLoggerCacheAddResult
+    internal class LoggingClientLoggerCacheAddResult : ILoggingClientLoggerCacheAddResult
     {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddResult" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static LoggingClientLoggerCacheAddResult() { }
+
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddResult" /> and
+        /// returns a reference to it.
+        /// </summary>
+        /// <remarks>
+        /// This is an empty, <see langword="private" /> constructor to prohibit direct
+        /// allocation of this class, as it is a <c>Singleton</c> object accessible via the
+        /// <see cref="P:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddResult.Instance" />
+        /// property.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        private LoggingClientLoggerCacheAddResult() { }
+
+        /// <summary>
+        /// Gets a reference to the one and only instance of the object that
+        /// implements the
+        /// <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheAddResult" />
+        /// interface.
+        /// </summary>
+        internal static ILoggingClientLoggerCacheAddResult Instance { [DebuggerStepThrough] get; } =
+            new LoggingClientLoggerCacheAddResult();
     }
 }
