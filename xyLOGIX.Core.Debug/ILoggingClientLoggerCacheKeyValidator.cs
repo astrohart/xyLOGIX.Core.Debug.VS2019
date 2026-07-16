8fd9a0337fd1e38ef7d57de8c1128293bdd085db
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 13:55:55 -0600

    Update ILoggingClientLoggerCacheKeyValidator.cs
    - Define the `ILoggingClientLoggerCacheKeyValidator` contract.
    - Add `IsValid(...)` to validate logging-client logger cache keys.
    - Document validation behavior and failure conditions for invalid cache keys.
    - Add XML documentation and the `NotLogged` annotation to the validator interface.

--- xyLOGIX.Core.Debug/ILoggingClientLoggerCacheKeyValidator.cs
+++ xyLOGIX.Core.Debug/ILoggingClientLoggerCacheKeyValidator.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a
+    /// component that validates logging-client logger-cache key object(s).
+    /// </summary>
     internal interface ILoggingClientLoggerCacheKeyValidator
     {
+        /// <summary>
+        /// Determines whether the specified <paramref name="cacheKey" /> contains
+        /// all the information required to identify a logger within a particular log4net
+        /// repository.
+        /// </summary>
+        /// <param name="cacheKey">
+        /// (Required.) Reference to an instance of an object that
+        /// implements the <see cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheKey" />
+        /// interface that is to be validated.
+        /// </param>
+        /// <remarks>
+        /// If a <see langword="null" /> reference is passed for the
+        /// <paramref name="cacheKey" /> parameter, then this method returns
+        /// <see langword="false" />.
+        /// <para />
+        /// This method also returns <see langword="false" /> if the
+        /// <see cref="P:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheKey.Repository" />
+        /// property has a <see langword="null" /> reference for a value or the
+        /// <see cref="P:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheKey.LoggerName" />
+        /// property is blank.
+        /// </remarks>
+        /// <returns>
+        /// <see langword="true" /> if the specified cache key is valid;
+        /// otherwise, <see langword="false" />.
+        /// </returns>
+        bool IsValid([NotLogged] ILoggingClientLoggerCacheKey cacheKey);
     }
 }
