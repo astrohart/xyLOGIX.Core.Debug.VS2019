90851c87a77a20d14a7f21e782ff858da77d8797
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 17:04:31 -0600

    Update LoggingClientLoggerCacheAddHandlerTypeValidator.cs
    - Implement the `IsValid(...)` validator entry point.
    - Add the initial handler-type validation method stub.
    - Document validation behavior for logging-client logger cache add-handler types.
    - Establish the implementation hook for subsequent enumeration validation logic.

--- xyLOGIX.Core.Debug/LoggingClientLoggerCacheAddHandlerTypeValidator.cs
+++ xyLOGIX.Core.Debug/LoggingClientLoggerCacheAddHandlerTypeValidator.cs
@@ -49,5 +49,25 @@ namespace xyLOGIX.Core.Debug
         {
             [DebuggerStepThrough] get;
         } = new LoggingClientLoggerCacheAddHandlerTypeValidator();
+
+        /// <summary>
+        /// Determines whether the logging-client logger cache <c>Add</c> handler
+        /// <paramref name="type" /> value passed is within the value set that is defined
+        /// by the
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddHandlerType" />
+        /// enumeration.
+        /// </summary>
+        /// <param name="type">
+        /// (Required.) One of the
+        /// <see cref="T:xyLOGIX.Core.Debug.LoggingClientLoggerCacheAddHandlerType" />
+        /// values that is to be examined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the logging-client logger cache <c>Add</c>
+        /// handler <paramref name="type" /> falls within the defined value set;
+        /// <see langword="false" /> otherwise.
+        /// </returns>
+        public bool IsValid(LoggingClientLoggerCacheAddHandlerType type)
+            => false;
     }
 }
