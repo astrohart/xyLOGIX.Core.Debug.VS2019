199aaeba278f97da697044880f4554a1d908bb80
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-16 16:58:35 -0600

    Update GetLoggingClientLoggerCacheAddHandlerType.cs
    - Implement the `GetLoggingClientLoggerCacheAddHandlerType` singleton factory.
    - Convert the placeholder class into a static factory type.
    - Add `SoleInstance()` to return the shared `ILoggingClientLoggerCacheAddHandlerTypeValidator` implementation.
    - Add XML documentation, logging attributes, and `DebuggerStepThrough` annotations.
    - Add defensive exception handling when resolving the singleton validator instance.

--- xyLOGIX.Core.Debug/GetLoggingClientLoggerCacheAddHandlerType.cs
+++ xyLOGIX.Core.Debug/GetLoggingClientLoggerCacheAddHandlerType.cs
@@ -1,12 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
 
 namespace xyLOGIX.Core.Debug
 {
-    internal class GetLoggingClientLoggerCacheAddHandlerType
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see
+    ///     cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheAddHandlerTypeValidator" />
+    /// interface.
+    /// </summary>
+    [Log(AttributeExclude = true)]
+    internal static class GetLoggingClientLoggerCacheAddHandlerType
     {
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Debug.GetLoggingClientLoggerCacheAddHandlerType" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetLoggingClientLoggerCacheAddHandlerType() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see
+        ///     cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheAddHandlerTypeValidator" />
+        /// interface, and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see
+        ///     cref="T:xyLOGIX.Core.Debug.ILoggingClientLoggerCacheAddHandlerTypeValidator" />
+        /// interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        internal static ILoggingClientLoggerCacheAddHandlerTypeValidator SoleInstance()
+        {
+            ILoggingClientLoggerCacheAddHandlerTypeValidator result;
+
+            try
+            {
+                result = LoggingClientLoggerCacheAddHandlerTypeValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the Debug output
+                System.Diagnostics.Debug.WriteLine(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
     }
 }
